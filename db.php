0422d4f0ecc5986c5df734c9190e00f5d1dfcf00
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2009-01-23 01:44:31 +0000

    Minor change.
    git-svn-id: https://cliniccases.googlecode.com/svn/trunk@155 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
-<? 
+<?
 # database connection scripts
 # the next 4 lines you can modify
 $dbhost = 'localhost';
-$dbusername = 'clinic';
-$dbpasswd = 'likes69';
-$database_name = 'cm_test';
+$dbusername = '';
+$dbpasswd = '';
+$database_name = '';
 
 #under here, don't touch!
-$connection = mysql_pconnect("$dbhost","$dbusername","$dbpasswd") 
+$connection = mysql_pconnect("$dbhost","$dbusername","$dbpasswd")
     or die ("Couldn't connect to server.");
 $db = mysql_select_db("$database_name", $connection)
     or die("Couldn't select database.");
