9ff27ea55f7369e732d9cf419929c2d6d79bf98c
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2009-09-09 10:59:13 +0000

    move old cc3 code to archive
    git-svn-id: https://cliniccases.googlecode.com/svn/trunk@403 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- journal_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-include 'db.php';
-$del_journal = mysql_query("UPDATE `cm_journals` SET `deleted` = 'yes' WHERE `id` = '$_GET[id]' LIMIT 1");
-$rand = rand();
-header('location: journal_list.php?notify=1&ieyousuck=' . $rand);
-
-?>
