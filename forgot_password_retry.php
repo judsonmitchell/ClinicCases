145720102ec3f26a3eae7fe444911d36557298de
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2009-09-09 10:54:51 +0000

    move old cc3 code to archive
    git-svn-id: https://cliniccases.googlecode.com/svn/trunk@395 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- forgot_password_retry.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-ECHO <<<RETRY
-<p>Please provide your email address and your information will be sent to you.</p>
-<input type = "text" id = "email" name="email" style="color:black;"><br>
-<a href="#" onClick = "createTargets('forgot','forgot');sendDataGet('forgot_password.php?email=' + document.getElementById('email').value);return false;"><img src="images/check_yellow.png" border="0" style="margin-top:15px;"></a>
-
-RETRY;
-?>
