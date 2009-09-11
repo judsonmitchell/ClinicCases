d6c1c2c60893ac5dad62b197bb10a9be82e38c69
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2009-09-11 12:36:12 +0000

    new add time feature in progress
    git-svn-id: https://cliniccases.googlecode.com/svn/trunk@479 54a900ba-8191-11dd-a5c9-f1483cedc3eb

---
+++ mobile/add_time_m.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+if (!$_SESSION){echo "You must be logged in to view this page.";die;}
+include '../db.php';
+?>
+
+<html>
+<head>
+<title>ClinicCases Mobile - Add Time</title>
+<link rel="stylesheet" href="mobile_style.css" type="text/css">
+</head>
+<body>
+<p><a href="cm_home_m.php"><img src="../images/logo_mobile.png"></a></p>
+<a class="nav"  href="cm_home_m.php">Main Menu</a> > <strong>Add Time</strong>
