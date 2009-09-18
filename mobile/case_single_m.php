5c73873a55383768b7e267d2c3e76809275bf1d2
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2009-09-18 12:31:14 +0000

    Add upcoming events
    git-svn-id: https://cliniccases.googlecode.com/svn/trunk@489 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- mobile/case_single_m.php
+++ mobile/case_single_m.php
@@ -16,10 +16,12 @@ include '../classes/format_dates_and_times.class.php';
 <body>
 <p><a href="cm_home_m.php"><img src="../images/logo_mobile.png" border="0"></a></p>
 
-<a href="cm_home_m.php">Main Menu</a> > <a href="cases_m.php">Results</a> >
+<a href="cm_home_m.php">Main Menu</a> > <a href="cases_m.php">Open Cases</a> >
 <strong><?PHP echo $r->first_name . ' ' . $r->last_name; ?> </strong><br>
+<p><a href="contact_m.php?type=client&id=<?php echo $r->id; ?>">Client Contact Info</a></p>
+<p><a href="case_contacts_m.php?id=<?php echo $r->id; ?>">Case Contacts</a></p>
 <p>Latest Activity</p>
-<table style="border:2px solid white;">
+<table>
 <?php
 	$q = mysql_query("SELECT * FROM `cm_case_notes` WHERE `case_id` = '$_GET[id]' ORDER BY `date` desc LIMIT 0,5");
 	while ($r = mysql_fetch_array($q))
@@ -27,15 +29,26 @@ include '../classes/format_dates_and_times.class.php';
 			echo "<tr><td  style='border-bottom:2px solid white;'valign='top' >" . formatDate2($r[date]) . "</td><td  style='border-bottom:2px solid white;'>$r[description]</td><td  style='border-bottom:2px solid white;' valign='top'>$r[username]</td></tr>";
 		}
 		if (mysql_num_rows($q)<1)
-		{echo "<p class='none'>No results found.</p>";}
+		{echo "<p class='no'>No results found.</p>";}
 ?>
 </table>
 <P>Upcoming Events</p>
 <table>
 <?php
 
+	$z = mysql_query("SELECT * FROM `cm_events` WHERE `case_id` = '$_GET[id]' and `archived` = 'n' AND `status` !='Done' ORDER BY `date_due` ASC");
+
+	while ($h = mysql_fetch_array($z))
+	{
+		echo "<tr><td style='border-bottom:2px solid white;'valign='top'>$h[task]</td><td style='border-bottom:2px solid white;'valign='top'>$h[date_due]</td></tr>";
+	}
+
+	if (mysql_num_rows($z)<1)
+		{echo "<p class='no'>No results found.</p>";}
+
 
 ?>
+</table>
 </table>
 </body>
 </html>
