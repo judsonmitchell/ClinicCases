fdd50774b57b8ba2f038ebb3e68e466ed16d9881
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2009-09-20 12:27:32 +0000

    Update rev on footer
    git-svn-id: https://cliniccases.googlecode.com/svn/trunk@500 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@ echo "
 <div style='margin-top:20px'><center>
 <font style='font-family:verdana,arial,helvetica;font-size:10pt;color:black'>Further Information: <a href='http://www.cliniccases.com'>ClinicCases.com</a></font><br>
 <font style='font-family:verdana,arial,helvetica;font-size:10pt;color:black'>&#169; 2007-2009 ClinicCases by </font><a style='font-family:verdana,arial,helvetica;font-size:10pt;text-decoration:underline;'href='http://www.threepipeproblem.com'>Three Pipe Problem</a> <font style='font-family:verdana,arial,helvetica;font-size:10pt;color:black'>of New Orleans</font><br>
-<font style='font-family:verdana,arial,helvetica;font-size:10pt;color:black'>ClinicCases 5 Rev 491 9.19.09</font>
+<font style='font-family:verdana,arial,helvetica;font-size:10pt;color:black'>ClinicCases 5 Rev 499 9.20.09</font>
 
 
 </center></div>";
