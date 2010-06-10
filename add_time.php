9d50ed923be47c1bc212c374f9c39b9d845fb650
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2010-06-10 15:19:00 +0000

    Implimenting multiple professors on one student: before time is entered, query is done to determine which professor or professors are assigned to the case.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc6@575 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- add_time.php
+++ add_time.php
@@ -28,7 +28,6 @@ else
 $username = $_SESSION['login'];
 $time = $_POST['time'];
 $description = $_POST['description'];
-$prof = $_SESSION['assigned_prof'];
 $case_id = $_POST['id'];
 $hours = $_POST['hours'];
 $minutes = $_POST['minutes'];
@@ -38,7 +37,16 @@ $hours_into_seconds = ($hours * 3600);
 $minutes_into_seconds = ($minutes * 60);
 $time = $hours_into_seconds + $minutes_into_seconds;
 }
-$query = mysql_query("INSERT INTO `cm_case_notes` (id,case_id,date,time,description,username,prof) VALUES (NULL,'$case_id',$new_date,'$time','$description','$username','$prof')");
+
+
+//A query is needed here to deal with multi-professor issue.  The question is: on this case into which the student is entering time, which professors are on this case?  If one, put in one, if two or more, put csv of all professors
+
+$profs_q = mysql_query("SELECT `id`,`professor` FROM `cm` WHERE `id` = '$case_id' LIMIT 1;");
+$profs_q2 = mysql_fetch_object($profs_q);
+$profs = $profs_q2->professor;
+
+
+$query = mysql_query("INSERT INTO `cm_case_notes` (id,case_id,date,time,description,username,prof) VALUES (NULL,'$case_id',$new_date,'$time','$description','$username','$profs')");
 
 setcookie("timeron", "timeroff");
 $rand = rand();
