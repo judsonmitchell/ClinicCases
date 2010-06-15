fbe06f1aaa1d3a6ddd0318c51318174eeef7728f
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2010-06-15 17:52:23 +0000

    Allows student with multiple professors to select to which professor the non-case time goes.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc6@594 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- casenote_noncase.php
+++ casenote_noncase.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
+include 'session_error.php';
 include 'db.php';
+include_once './classes/get_names.php';
+
 
 /* This is a hack to fix an ajax problem with the calendar coming back after you navigate away from this page.  The date input is given a unix timestamp to make it unique.  Solves DOM problem with javascript */
 $date = time();
@@ -12,12 +15,49 @@ $date = time();
 <div style="border:1px solid gray;width:95%;height:87%;margin:0% 2% 2.5% 2%;">
 
 <div id="time_form" style="width:100%;height:100px;background-color:rgb(255, 255, 204);z-index:3;position:relative;>
+<?php
+//Find out if student has multiple professors.
+	
+	$prof_q = mysql_query("SELECT `username`,`assigned_prof` FROM `cm_users` WHERE `username` = '$_SESSION[login]' LIMIT 1");
+	$line = mysql_fetch_object($prof_q);
+	$prof_string_f = $line->assigned_prof;
+	$prof_string = substr($prof_string_f,0,-1);
+	$prof_arr = explode(",",$prof_string);
+	$size = count($prof_arr);
+		
+		if ($size > 1)
+			{
+				$prof_chooser = "<select name='professor'><option selected='selected'>--</option>";
+				foreach ($prof_arr as $p)
+					{
+						$fn = new get_names;$full_name=$fn->get_users_name_initial($p);
+						$prof_chooser .= "<option value='$p,'>$full_name</option>";
+					}
+				
+				$prof_chooser .= "</select>";
+				echo $prof_chooser;
+			
+			}
+			
+			else
+			
+			{ 
+				
+				$prof_chooser =  "<input type='hidden' name='professor' value='$prof_string_f'>";	
+			}
 
 
-<form id="caseNotes">
+?>
+
+<form id="time_form">
 <table id="time_form_table" border="0" >
 <tr>
-<td></td><td><label for "date">Select Date</label></td><td><label for "hours">Hours</label></td><td><label for "minutes">Minutes</label><td align="center"><label for "description">Describe What You Did</label></td><td></td><td></td></tr>
+<td></td><td><label for "date">Select Date</label></td><td><label for "hours">Hours</label></td><td><label for "minutes">Minutes</label><td align="center"><label for "description">Describe What You Did</label></td><td>
+<?php
+if ($size > 1)
+{echo "<label for 'professor'>Which professor did you do this for?</label>";}
+?>
+</td><td></td></tr>
 <tr><td><input type="text" id="date<?php echo $date; ?>" name="date" size="10" style="border:0px;background-color:rgb(255, 255, 204);"></td><td align="center">
 
 
@@ -48,6 +88,8 @@ $date = time();
 </select>
 
 </td><td><textarea cols="38" rows="2" id="description" name="description"></textarea></td><td><input type="hidden" name="id" value="NC">
+<?php echo $prof_chooser; ?>
+</td><td>
 <a href="#" onClick="var check = forceDate('date<?php echo $date; ?>');if (check == true){createTargets('status','the_info');sendDataPostAndStripeNoStatus2('add_time.php','time_form');clearForm();return false;}" alt="Add This Case Note" title="Add This Case Note"><img src="images/check_yellow.png" border="0"></a></td></tr></table></form></div>
 <div id = "the_info" style="height:60%;width:100%;">
 <?php
