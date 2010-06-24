af25f4a1404d716d72225b3074a5fde190dffdaa
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2010-06-24 16:47:58 +0000

    Add script to generate student thumbnails next to case file on Students page
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc6@623 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- classes/get_faces.php
+++ classes/get_faces.php
@@ -27,9 +27,19 @@ function get_faces($id,$output)
 				$thumb = explode('/',$pn_l->picture_url);
 				$thumb_target = $thumb[0] . '/tn_' . $thumb[1];
 				
-				$jscript = "'<img src=\'images/cross.png\' border=\'0\'>'";
-				$faces .= "<a onmouseover=\"$('$unique').setStyle({width:'50px'});\"  onmouseout=\"$('$unique').setStyle({width:'30px'});\" onClick=\"var check = confirm('Are sure you want to remove $pn_l->first_name $pn_l->last_name from this case?');if (check==true){INSERT CODE}else{}\" title='$pn_l->first_name $pn_l->last_name' alt='$pn_l->first_name $pn_l->last_name'><div id=\"$unique\" style=\"width:33px;height:32px;float:left;margin:5px;background:url('images/tn_del.png') no-repeat;\"><img src='$thumb_target' border='0'></div></a>";
+				$date_assigned = formatDate2($l[date_assigned]);
+				
+				$faces .= "<a class=\"tooltip\" onmouseover=\"$('s_$unique').setStyle({display:'inline',position:'absolute', background:'#ffffff',border:'1px solid #555' ,color:'#6c6c6c'});\" onmouseout=\"$('s_$unique').hide();\" onClick=\"var check = confirm('Are sure you want to remove $pn_l->first_name $pn_l->last_name from this case?');
+				
+				if (check==true){new Ajax.Updater('notifications','./student_remove_from_case.php',{method:'get',parameters:{username:'$l[username]',case_id:'$l[case_id]'},onComplete:function(){new Ajax.Updater($('facebar_$id'),'./classes/get_faces.php',{evalScripts:true,method:'get',parameters:{id:'$id'}})
+				}});}else{return false;}\">
+			
+				<div id=\"$unique\" style=\"width:33px;height:32px;float:left;margin:5px;\">
+					<span class=\"s_tooltip\" id=\"s_$unique\">$pn_l->first_name $pn_l->last_name<br><p>Assigned: $date_assigned</p> <p style=\" color:red;\">[Remove]</p></span>
+				<img src='$thumb_target' border='0'></div></a>";
 			}
+			
+			$faces .="<script>  $$('span.s_tooltip').invoke('hide');</script>";
 			
 		if (mysql_num_rows($f)<1)
 		{$faces = "<span style='font-style:italic;color:gray;'>No current students assigned to this case.</span>";}
@@ -43,5 +53,6 @@ function get_faces($id,$output)
 
 //create a facebar by calling this script with a _get value	
 if (isset($_GET['id']))
-	{include_once '../db.php';$id = $_GET['id'];$output= "text";get_faces($id,$output);}
+	{include_once '../db.php';include_once 'format_dates_and_times.class.php';
+$id = $_GET['id'];$output= "text";get_faces($id,$output);}
 ?>
