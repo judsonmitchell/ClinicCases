5d454761e22638b3a65aaf5871162f1a8c599a09
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2010-06-25 16:40:53 +0000

    correction to upgrade script when updating professor fields
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc6@626 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- _INSTALL/upgrade_to_cc6.php
+++ _INSTALL/upgrade_to_cc6.php
@@ -7,30 +7,42 @@ include '../classes/thumbnail_generator.php';
 
 //get the assigned professor names from professor and professor 2
 
+echo "Updating the professors field...<br>";
 
-$get_profs = mysql_query("SELECT `id`,`professor`,`professor2` from `cm`");
+$get_profs = mysql_query("SELECT `id`,`professor`,`professor2` from `cm`") or die("Query failed with error: ".mysql_error());
+
+	
 
 //loop through the array, putting all values in professor in a comma seperated list.
 
 while ($r = mysql_fetch_array($get_profs))
 	{
 		//note that the last comma is needed b/c scripts assume a trailing comma:
-		$data = $r[professor] . ',' . $r[professor2] . ',';
+		if (!empty($r['professor2']))
+			{$data = $r['professor'] . ',' . $r['professor2'] . ',';}
+			else
+			{$data = $r['professor'] . ',';}
+			
 		$update = mysql_query("UPDATE `cm` SET `professor` = '$data' WHERE `id` = '$r[id]' LIMIT 1");	
 		
 	}
 
-//error
-if (mysql_errno($connection)) { 
-  $error = "Sorry, there was an error updating the professor fields.  Maybe this will help: MySQL error ". mysql_errno($connection).": ". mysql_error($connection)."\n<br>When executing:<br>\n$get_profs\n<br>";die;
+	//error
+	if (mysql_errno($connection)) { 
+	  $error = "Sorry, there was an error updating the professor fields.  Maybe this will help: MySQL error ". mysql_errno($connection).": ". mysql_error($connection)."\n<br>When executing:<br>\n$get_profs\n<br>";die;
+	}
+
+
+
 
 //delete column professor2
-}
-$delete_col = mysql_query("ALTER TABLE `cm` DROP COLUMN `professor2`");
 
-if (mysql_errno($connection)) { 
-  $error = "Sorry, there was an error deleting the second professor field.  Maybe this will help: MySQL error ".mysql_errno($connection).": ". mysql_error($connection)."\n<br>When executing:<br>\n$delete_col\n<br>"; die;
-}
+$delete_col = mysql_query("ALTER TABLE `cm` DROP COLUMN `professor2`");
+	
+	//error
+	if (mysql_errno($connection)) { 
+	  $error = "Sorry, there was an error deleting the second professor field.  Maybe this will help: MySQL error ".mysql_errno($connection).": ". mysql_error($connection)."\n<br>When executing:<br>\n$delete_col\n<br>"; die;
+	}
 echo "Assigned Professors updated....<br>Creating thumbnails<br>";
 
 
