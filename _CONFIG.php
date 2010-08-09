60e19511fb5ef773c95ea4b871370c470927169c
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2010-08-09 15:05:24 +0000

    Clean _CONFIG
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc6@661 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- _CONFIG.php
+++ _CONFIG.php
@@ -2,25 +2,25 @@
 // SETS SYSTEM-WIDE CONFIG PREFERNENCES
 
 //Information to access your mysql database
-$CC_dbhost = "localhost";
-$CC_dbusername = "cc6";
-$CC_dbpasswd = "likes69";
-$CC_database_name = "cc6";
+$CC_dbhost = "";
+$CC_dbusername = "";
+$CC_dbpasswd = "";
+$CC_database_name = "";
 
 //Email address for the adminstrator who will deal with student questions
-$CC_admin_email = "mitchell@threepipeproblem.com";
+$CC_admin_email = "";
 
 //Url where your Cliniccases installation will be run, e.g. http://www.yourservername.com/yourdirectory/ .  Don't forget trailing slash!
-$CC_base_url = "http://localhost/cc6_svn";
+$CC_base_url = "";
 
 //Your domain , e.g. http://www.yourserver.com
-$CC_domain = "http://localhost";
+$CC_domain = "";
 
-//Default email for ClinicCases notifications, e.g. no-reply@yourserver.com
-$CC_default_email = "no-reply@cliniccases.com";
+//Default email for ClinicCases notifications, e.g. no-reply@yourserver.co
+$CC_default_email = "";
 
 //Your Program Name, e.g. "Loyola Law Clinic"
-$CC_program_name = "cc6 Testing";
+$CC_program_name = "";
 
 
 
