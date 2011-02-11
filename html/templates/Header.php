3442ebeb46bec436f7f6169f64ba2d5bec2116ac
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-02-11 14:08:44 +0000

    Changes to impliment jquery idle timeout
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@735 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- html/templates/Header.php
+++ html/templates/Header.php
@@ -5,6 +5,9 @@
 	<meta name="robots" content="noindex">
 	<link rel="stylesheet" href="html/css/cm.css" type="text/css">
 	<link rel="stylesheet" href="html/css/cm_tabs.css" type="text/css">
+	<link rel="stylesheet" href="lib/jqueryui/css/custom-theme/jquery-ui-1.8.9.custom.css" type="text/css">
 	<script src="lib/jqueryui/js/jquery-1.4.4.min.js" type="text/javascript"></script>
 	<script src="lib/jqueryui/js/jquery-ui-1.8.9.custom.min.js" type="text/javascript"></script>
+	<script src="lib/javascripts/jquery.idletimer.js" type="text/javascript"></script>
+	<script src="lib/javascripts/jquery.idletimeout.js" type="text/javascript"></script>
 
--- html/templates/Home.php
+++ html/templates/Home.php
@@ -4,18 +4,36 @@
 		
 		$(document).ready(function(){
 			
+			$.idleTimeout('#idletimeout', '#idletimeout a', {
+				idleAfter: 5,
+				pollingInterval: 2,
+				keepAliveURL: 'lib/php/keep_alive.php',
+				serverResponseEquals: 'OK',
+				onTimeout: function(){
+					$(this).slideUp();
+					window.location = "html/templates/Logout.php";
+					},
+						onIdle: function(){
+						$(this).slideDown(); // show the warning bar
+									},
+						onCountdown: function( counter ){
+						$(this).find("span").html( counter ); // update the counter
+							},
+						onResume: function(){
+						$(this).slideUp(); // hide the warning bar
+						}
+				});
 			
-			
-			
-			
-			
-		})
+})
 	</script>
 </head>
 <body>
 
 	<div id="notifications"></div>
-
+	<div id="idletimeout">
+	You will be logged off in <span><!-- countdown place holder --></span>&nbsp;seconds due to inactivity. 
+	<a id="idletimeout-resume" href="#">Click here to continue using ClinicCases</a>.
+</div>
 	<div id = "nav_container">
 
 		<?php $t = tabs($_GET['i']); echo $t; ?>
