6558e6e74001d3a7bda891c1fa8871e6917f0e51
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-02-14 19:09:34 +0000

    Add functions to deal with dates and times.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@757 54a900ba-8191-11dd-a5c9-f1483cedc3eb

---
+++ lib/php/data/time.php
@@ -0,0 +1,15 @@
+<?php
+
+//This file includes all functions which deal with formatting dates and times.
+
+function sql_date_to_us_date ($date)
+
+	{
+		
+		$parts = explode('-', $date);
+		$us_date = $parts[1] . "/" . $parts[2] . "/" . $parts[0];
+		return $us_date;
+						
+		
+	}
+
