77e67a420e123763ebc62d63373a63d04eba0def
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-02-15 13:06:03 +0000

    Remove duplicate css link
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@764 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- html/templates/Header.php
+++ html/templates/Header.php
@@ -8,8 +8,7 @@
 	<link rel="stylesheet" href="lib/jqueryui/css/custom-theme/jquery-ui-1.8.9.custom.css" type="text/css">
 	<link rel="stylesheet" href="lib/DataTables-1.7.5/media/css/data_table.css" type="text/css">
 	<link rel="stylesheet" href="lib/DataTables-1.7.5/extras/TableTools-2.0.0/media/css/TableTools.css" type="text/css">
-	
-	<link rel="stylesheet" href="lib/jqueryui/css/custom-theme/jquery-ui-1.8.9.custom.css" type="text/css">
+
 	<script src="lib/jqueryui/js/jquery-1.4.4.min.js" type="text/javascript"></script>
 	<script src="lib/jqueryui/js/jquery-ui-1.8.9.custom.min.js" type="text/javascript"></script>
 	<script src="lib/javascripts/jquery.idletimer.js" type="text/javascript"></script>
