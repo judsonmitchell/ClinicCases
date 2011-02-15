51c717f21d6c756648ff41cb4d04d54f679ba0f9
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-02-15 01:44:18 +0000

    Add  user-friendly formatting to data returned from cases query
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@759 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- lib/php/data/cases_load.php
+++ lib/php/data/cases_load.php
@@ -2,6 +2,7 @@
 session_start();
 include '../../../db.php';
 include 'time.php';
+include 'names.php';
 
 	$user = $_SESSION['login'];
 	
@@ -9,7 +10,7 @@ include 'time.php';
 		{
 		
 		case 'prof':
-		$query = mysql_query("SELECT `id`, `first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`dispo` FROM `cm` WHERE `professor` LIKE '%$user%'");
+		$query = mysql_query("SELECT `id`, `first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`dispo` FROM `cm` WHERE MATCH(`professor`) AGAINST ('$user')");
 		break;
 		
 		case 'admin':
@@ -25,25 +26,49 @@ include 'time.php';
 		break;			
 			
 		}
+			
 
-	
-	
 	$cols = array('id','first_name','last_name','date_open','date_close','case_type','professor','dispo');
 	
 		while ($result = mysql_fetch_assoc($query))
 			{
 				
 				$rows = array();
-				//for ( $i=0 ; $i<count($cols) ; $i++ )
 				
+			//perform some formatting to make the data more user-friendly
+				
+				//Convert dates
+				if (!empty($result['date_open']));
+					{
+						$result['date_open'] = sql_date_to_us_date($result['date_open']);
+					}
+			
+				if (!empty($result['date_close']))
+					{			
+						$result['date_close'] = sql_date_to_us_date($result['date_close']);
+					}
+			
+				//Convert username into last name
+				if (!empty($result['professor']))
+					{
+						$profs = explode(",",$result['professor']);
+						array_pop($profs);
+						array_walk($profs,'username_to_lastname');
+						$result['professor'] = implode(", ",$profs);
+					}
+					
+					
+			//loop through results, create array, convert to json	
 				foreach ($cols as $col)
 					{
+						
 						$rows[] = $result[$col];
 					}	
 				
 				$output['aaData'][] = $rows;
 
 			}
+
 	
 	$json = json_encode($output);
 	echo $json;
