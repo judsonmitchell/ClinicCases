86dd4e41b2a7233d853ff0eb913039ff19e19c8e
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-02-17 13:28:23 +0000

    Add additional fields to data array. (e.g., gender, race)
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@770 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- lib/php/data/cases_load.php
+++ lib/php/data/cases_load.php
@@ -10,25 +10,25 @@ include 'names.php';
 		{
 		
 		case 'prof':
-		$query = mysql_query("SELECT `id`, `first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`dispo` FROM `cm` WHERE MATCH(`professor`) AGAINST ('$user')");
+		$query = mysql_query("SELECT `id`, `clinic_id`,`first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`ssn`,`dob`,`age`,`gender`,`race`,`dispo` FROM `cm` WHERE MATCH(`professor`) AGAINST ('$user')");
 		break;
 		
 		case 'admin':
-		$query = mysql_query("SELECT `id`, `first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`dispo` FROM `cm`");
+		$query = mysql_query("SELECT `id`, `clinic_id`,`first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`ssn`,`dob`,`age`,`gender`,`race`,`dispo` FROM `cm`");
 		break;
 		
 		case 'student':
-		$query = mysql_query("SELECT cm.id, cm.first_name, cm.last_name, cm.date_open, cm.date_close, cm.case_type, cm.professor, cm.dispo, cm_cases_students.case_id, cm_cases_students.username FROM cm, cm_cases_students WHERE cm.id = cm_cases_students.case_id AND cm_cases_students.username =  '$user' AND cm_cases_students.status =  'active'");
+		$query = mysql_query("SELECT cm.id, cm.clinic_id,cm.first_name, cm.last_name, cm.date_open, cm.date_close, cm.case_type, cm.professor, cm.ssn,cm.dob,cm.age,cm.gender,cm.race,cm.dispo, cm_cases_students.case_id, cm_cases_students.username FROM cm, cm_cases_students WHERE cm.id = cm_cases_students.case_id AND cm_cases_students.username =  '$user' AND cm_cases_students.status =  'active'");
 		break;
 		
 		case 'super':
-		$query = mysql_query("SELECT `id`, `first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`dispo` FROM `cm`");
+		$query = mysql_query("SELECT `id`, `clinic_id`,`first_name`,`last_name`,`date_open`,`date_close`,`case_type`,`professor`,`ssn`,`dob`,`age`,`gender`,`race`,`dispo` FROM `cm`");
 		break;			
 			
 		}
 			
 
-	$cols = array('id','first_name','last_name','date_open','date_close','case_type','professor','dispo');
+	$cols = array('id','clinic_id','first_name','last_name','date_open','date_close','case_type','professor','ssn','dob','age','gender','race','dispo');
 	
 		while ($result = mysql_fetch_assoc($query))
 			{
