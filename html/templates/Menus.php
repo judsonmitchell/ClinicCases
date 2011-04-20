c8c85597c8cb51f7bf52de18882a93e34c3c3670
judsonmitchell <judsonmitchell@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-04-20 17:08:31 +0000

    Set token for a user-directed logout.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@793 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- html/templates/Menus.php
+++ html/templates/Menus.php
@@ -1,1 +1,1 @@
-<img src="html/images/logo_small4.png">  <a class="menu" href="http://code.google.com/p/cliniccases/issues/entry" target="_new" >Report Problems</a> | <a class="menu" href="html/templates/Logout.php">Logout</a> 
+<img src="html/images/logo_small4.png">  <a class="menu" href="http://code.google.com/p/cliniccases/issues/entry" target="_new" >Report Problems</a> | <a class="menu" href="html/templates/Logout.php?user=1">Logout</a> 
