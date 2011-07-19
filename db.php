3fdb2ef9c919877041775abbfd224b6b60838494
judsonmitchell@gmail.com <judsonmitchell@gmail.com@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-07-19 15:44:20 +0000

    Clean up db file, remove deprecated code.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@826 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- db.php
+++ db.php
@@ -1,10 +1,8 @@
-<?
-# database connection scripts
+<?php
 include '_CONFIG.php';
 
-#under here, don't touch!
 $connection = mysql_pconnect("$CC_dbhost","$CC_dbusername","$CC_dbpasswd")
     or die ("Couldn't connect to server.");
 $db = mysql_select_db("$CC_database_name", $connection)
     or die("Couldn't select database.");
-?>
+
