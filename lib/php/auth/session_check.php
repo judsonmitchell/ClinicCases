9750b541baccfbce7490ade993f562e64205bccc
judsonmitchell@gmail.com <judsonmitchell@gmail.com@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-09-12 10:25:45 +0000

    Checks to ensure that the user is logged in and that the session comes from ClinicCases
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@848 54a900ba-8191-11dd-a5c9-f1483cedc3eb

---
+++ lib/php/auth/session_check.php
@@ -0,0 +1,9 @@
+<?php
+
+	//Existence of session permissions indicates that the user is validly logged in to a ClinicCases session on the server	
+		
+	if (!isset($_SESSION['permissions']))
+
+		{echo "You don't have permission to view this page.  Please <a href=\"index.php\">log in.</a>";die;}
+		
+
