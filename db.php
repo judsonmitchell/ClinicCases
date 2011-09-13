f8170602b2f221608e71078839d02efb50b0a34a
judsonmitchell@gmail.com <judsonmitchell@gmail.com@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-09-13 22:52:33 +0000

    Add code for PDO connects to ms sql and sqlite, fix comments.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@858 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- db.php
+++ db.php
@@ -4,11 +4,11 @@ include '_CONFIG.php';
 try {
 		$dbh = new PDO("mysql:host=$CC_dbhost;dbname=$CC_database_name", $CC_dbusername, $CC_dbpasswd);
 		
-		 # MS SQL Server and Sybase with PDO_DBLIB
-			#$dbh = new PDO("mssql:host=$CC_dbhost;dbname=$CC_database_name, $CC_dbusername, $CC_dbpasswd");
-			#$dbh = new PDO("sybase:host=$CC_dbhost;dbname=$CC_database_name, $CC_dbusername, $CC_dbpasswd");
+		 // MS SQL Server and Sybase with PDO_DBLIB
+			//$dbh = new PDO("mssql:host=$CC_dbhost;dbname=$CC_database_name, $CC_dbusername, $CC_dbpasswd");
+			//$dbh = new PDO("sybase:host=$CC_dbhost;dbname=$CC_database_name, $CC_dbusername, $CC_dbpasswd");
 			
-		# SQLite Database  
+		// SQLite Database  
 			#$dbh = new PDO("sqlite:my/database/path/database.db");  		
 		
     }
