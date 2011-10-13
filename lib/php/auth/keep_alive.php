0cc652a4ce7b0146a9b8ba4a3d922971af0217bb
judsonmitchell@gmail.com <judsonmitchell@gmail.com@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-10-13 12:50:23 +0000

    Now checks for unique session id.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@884 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- lib/php/auth/keep_alive.php
+++ lib/php/auth/keep_alive.php
@@ -1,7 +1,7 @@
 <?php 
 session_start();
 
-	if (!isset($_SESSION['permissions']))
+	if (!isset($_SESSION['cc_session_id']))
 		//this header will trigger an ajax error and warn the user
 		{header("HTTP/1.0 401 Not Authorized");}
 		
--- lib/php/auth/session_check.php
+++ lib/php/auth/session_check.php
@@ -2,7 +2,7 @@
 
 	//Existence of session permissions indicates that the user is validly logged in to a ClinicCases session on the server	
 		
-	if (!isset($_SESSION['permissions']))
+	if (!isset($_SESSION['cc_session_id']))
 
 		{echo "You don't have permission to view this page.  Please <a href=\"index.php\">log in.</a>";die;}
 		
