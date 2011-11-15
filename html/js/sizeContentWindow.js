69bf8df622edcfe9af4544eaef8f3d2ee5292d56
judsonmitchell@gmail.com <judsonmitchell@gmail.com@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-11-15 12:19:59 +0000

    Sets the size of the content window to always be within viewport; will be necessary when case tabs minimize to bottom of content window.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@899 54a900ba-8191-11dd-a5c9-f1483cedc3eb

---
+++ html/js/sizeContentWindow.js
@@ -0,0 +1,25 @@
+//Sizes the content window (#content) so that the bottom is always visible.
+
+//Get height of the viewport
+var viewportHeight = $(window).height();
+
+//Determine the percentage height.
+
+var adjustedHeight = Math.round(viewportHeight * .80);
+
+//set the css after load
+$(document).ready(function(){
+	$("#content").css({'height':adjustedHeight + 'px'});
+	
+})
+
+//handle if user resizes window
+
+$(window).resize(function() {
+	
+	var newViewportHeight = $(window).height();
+	var newAdjustedHeight = Math.round(newViewportHeight * .80);
+	$("#content").css({'height':newAdjustedHeight + 'px'});
+
+
+});
