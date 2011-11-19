5202ee6046303ac8a62130d06966d0daa0f59e4f
judsonmitchell@gmail.com <judsonmitchell@gmail.com@54a900ba-8191-11dd-a5c9-f1483cedc3eb>
2011-11-19 16:15:26 +0000

    Further changes on case content window.
    git-svn-id: https://cliniccases.googlecode.com/svn/branches/cc7@910 54a900ba-8191-11dd-a5c9-f1483cedc3eb

--- html/js/viewCaseDetail.js
+++ html/js/viewCaseDetail.js
@@ -4,11 +4,40 @@ function addDetailTabs(id)
 
 	{
 
+		$(function() {
+			
+			
+			$.getJSON("lib/php/data/case_detail_tab_case_name.php?id=" + id,function(data){
+				if (data.organization.length<1)
+				{tabData = data.last_name + ", " + data.first_name}
+				else
+				{tabData = data.organization}
+				
+				if (tabData.length>15)
+				{tabData = tabData.substring(0,15) + "..."}
+				
+				$("#case_detail_tab_row").tabs("add","people/index.php",tabData);			
+				});
+			
+			//$("#case_detail_tab_row").tabs("add","index.php","Yahoo");
+			//$("#case_detail_tab_row").tabs("add","index.php","Google");
+			
+			$(".ui-widget-content").css({'border':'0px'})
+			$(".ui-tabs").css({'padding':'0px'});
+			//make tabs smaller
+			$(".ui-helper-reset").css({'line-height':'0.3'});
+			//make buttons smaller
+			$(".ui-button-text").css({'line-height':'0.3'});
 
-
-
+		})
 
 	}
+
+//Close tabs	
+$( "span.ui-icon-close" ).live( "click", function() {
+			var index = $( "li", $tabs ).index( $( this ).parent() );
+			$tabs.tabs( "remove", index );
+		});
 	
 //Function which creates the case detail window.
 	
@@ -16,17 +45,24 @@ function callCaseWindow(id)
 
 	{
 		//create window if user has yet to call it
-		if($("#case_detail_block").length<1)
+		if($("#case_detail_window").length<1)
 		{
-			var caseDetail = "<div id='case_detail_window'><div id='case_detail_tab_row'><div id='case_detail_control'></div></div><div id='case_detail_block'></div></div>";
+			var caseDetail = "<div id='case_detail_window'><div id='case_detail_tab_row'><ul></ul><div id='case_detail_control'></div></div></div>";
 			
 			$("#content").append(caseDetail);
 			
 			$("#case_detail_window").hide().show('clip',1200);
+						
+			$("#case_detail_control").html("<button>Minimize</button><button>Close</button>");
+			$("#case_detail_control button:first").button({icons: {primary: "fff-icon-arrow-in"},
+            text: true}).next().button({icons: {primary: "fff-icon-cancel"},text:true})
 			
-			$("#case_detail_tab_row").css({opacity: 0.8 }).addClass("ui-corner-all");;
+			$tabs = $("#case_detail_tab_row").tabs({tabTemplate: "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>"});
 			
-			$("#case_detail_control").html("<img id='control_down' src='html/images/downarrow.png'><img id='control_cancel' src='html/images/cancel.png'>");
+			// this creates sortable, but causes the close tab to not work - .find( ".ui-tabs-nav" ).sortable({ axis: "x" })
+			
+			addDetailTabs(id);
+
 
 		}	
 		
@@ -35,12 +71,15 @@ function callCaseWindow(id)
 		{
 			
 			$("#case_detail_window").hide().show('clip',1200);
+			addDetailTabs(id);
+
 			
-		}
-			
+		}		
 		
 	}
 	
 //Listeners
 
-$("#control_cancel").live('click',function(){$("#case_detail_window").hide('clip',2000);});
+$("#case_detail_control button:first").live('click',function(){alert('minimize')});
+
+$("#case_detail_control button + button").live('click',function(){$("#case_detail_window").hide('clip',2000);});
