f23522a895ae3dcf1bbebb37ff3f228f807bbb00
Judson Mitchell <judsonmitchell@gmail.com>
2011-12-21 15:32:49 -0600

    Reset config file.

--- _CONFIG.php
+++ _CONFIG.php
@@ -3,27 +3,27 @@
 
 //Information to access your database
 define("CC_DBHOST","localhost");
-define("CC_DBUSERNAME","cc7");
-define("CC_DBPASSWD", "cc7");
-define("CC_DATABASE_NAME","cc_dev_live_copy");
+define("CC_DBUSERNAME","");
+define("CC_DBPASSWD", "");
+define("CC_DATABASE_NAME","");
 
 //If sqlite, define path to db
 //define("CC_SQLITE_PATH","my/database/path/database.db");
 
 //Email address for the adminstrator who will deal with student questions
-define("CC_ADMIN_EMAIL","jmitchel@loyno.edu");
+define("CC_ADMIN_EMAIL","");
 
 //Url where your Cliniccases installation will be run, e.g. http://www.yourservername.com/yourdirectory/ .  Don't forget trailing slash!
-define("CC_BASE_URL","http://192.168.1.110/cc7/cliniccases/");
+define("CC_BASE_URL","");
 
 //Your domain , e.g. http://www.yourserver.com
-define("CC_DOMAIN","http://192.168.1.110");
+define("CC_DOMAIN","");
 
 //Default email for ClinicCases notifications, e.g. no-reply@yourserver.co
 define("CC_DEFAULT_EMAIL","");
 
 //Your Program Name, e.g. "Loyola Law Clinic"
-define("CC_PROGRAM_NAME","ClinicCases 7 Development git");
+define("CC_PROGRAM_NAME","");
 
 //Minimum timekeeping unit.  Default is 5 minutes.  Some prefer 6
 define("CC_TIME_UNIT","5");
