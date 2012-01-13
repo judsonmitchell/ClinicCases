f162357b2bdb9cc0bd6df458e88c4e3b0af929df
Judson Mitchell <judsonmitchell@gmail.com>
2012-01-13 07:29:49 -0600

    Complete in place editing of case notes.

--- html/templates/interior/cases_casenotes.php
+++ html/templates/interior/cases_casenotes.php
@@ -3,15 +3,15 @@ if (!isset($_POST['update']))
 {echo<<<TOOLS
 
 		<div class="user_display ui-widget ui-widget-content ui-corner-bottom user_widget">
-						
+
 		</div>
-		
+
 		<div class="case_detail_panel_tools">
-		
+
 			<div class="case_detail_panel_tools_left"></div>
-			
+
 			<div class="case_detail_panel_tools_right">
-			
+
 				<input type="text" class="casenotes_search" id="casesearch_
 TOOLS;
 echo $case_notes_data[0]['case_id'];
@@ -22,10 +22,10 @@ echo <<<TOOLS
 TOOLS;
 
 			if ($_SESSION['permissions']['add_case_notes'] == '1')
-			{echo "<button class = \"button1\">Add</button>	
+			{echo "<button class = \"button1\">Add</button>
 			<button class = \"button2\">Timer</button>";
 			}
-				
+
 echo <<<TOOLS
 
 				<button class = "button3">Print</button>
@@ -33,13 +33,13 @@ echo <<<TOOLS
 			</div>
 
 		</div>
-		
+
 		<div class="case_detail_panel_casenotes case_
 TOOLS;
 echo $case_notes_data[0]['case_id'] . "\">";
 }
 //new note form to be hidden
-		
+
 			$this_thumb = thumbify($_SESSION['picture_url']);
 			$this_date = date('n/j/Y');
 			$this_fname = $_SESSION['first_name'];
@@ -52,7 +52,7 @@ echo <<<NEWNOTE
 <div class='csenote csenote_new'>
 			<form>
 			<div class='csenote_bar'>
-				<div class = 'csenote_bar_left'><img src='$this_thumb'> $this_fname $this_lname</div> 
+				<div class = 'csenote_bar_left'><img src='$this_thumb'> $this_fname $this_lname</div>
 				<div class = 'csenote_bar_right'>
 				<label>Date:</label> <input type='hidden' name='csenote_date' class='csenote_date_value' value='$this_date'> $selector
 				<input type='hidden' name='csenote_user' value='$this_user'>
@@ -66,27 +66,25 @@ echo <<<NEWNOTE
 
 NEWNOTE;
 
-		
-						
 			//show all case notes
-		
+
 			foreach($case_notes_data as $case_notes)
 			{
-				
+
 				$time = convert_case_time($case_notes['time']);
 				echo "<div class='csenote' id='csenote_" . $case_notes['id'] . "'>
 				<div class='csenote_bar'>
-				<div class = 'csenote_bar_left'><img src='" . thumbify($case_notes['picture_url']) . "'> " . username_to_fullname($dbh,$case_notes['username']). "</div><div class = 'csenote_bar_right'>" . extract_date($case_notes['datestamp']) .  " &#183; " . $time[0] . $time[1];
-				
+				<div class = 'csenote_bar_left'><img src='" . thumbify($case_notes['picture_url']) . "'> " . username_to_fullname($dbh,$case_notes['username']). "</div><div class = 'csenote_bar_right'><span class='csenote_date'>" . extract_date($case_notes['date']) .  "</span> &#183; <span class='csenote_time'>" . $time[0] . $time[1] . "</span>";
+
 				if ($case_notes['username'] == $_SESSION['login'])
 				{echo " &#183; <a href='#' class='csenote_edit'>Edit</a> <a href='#' class='csenote_delete'>Delete</a>";}
-				
+
 				echo "</div></div><p class='csenote_instance'>"    . $case_notes['description'] . "</p></div>";
-				
+
 			}
-			
+
 			if (empty($case_notes_data))
-				{echo "<p>No case notes found.</p>";}		
-		
+				{echo "<p>No case notes found.</p>";}
+
 		if (!isset($_POST['update'])){echo "</div>";}
 
