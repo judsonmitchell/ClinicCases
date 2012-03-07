0768c8f1bb53fcdaa0297c916e453539136e8417
Judson Mitchell <judsonmitchell@gmail.com>
2012-03-06 19:58:21 -0600

    Further changes to case contacts

--- html/templates/interior/cases_contacts.php
+++ html/templates/interior/cases_contacts.php
@@ -10,13 +10,29 @@
 
 		<input type="text" class="contacts_search" value="Search Contacts">
 
-		<button class='new_contact'>New Contact</button>
+<?php
+	if ($_SESSION['permissions']['add_contacts'] == '1')
+		{echo "<button class='new_contact'>New Contact</button>";}
+
+?>
+
 
 	</div>
 
 </div>
 
 <div class="case_detail_panel_casenotes">
+
+	<div class='csenote csenote_new'>
+			<form>
+			<div class='csenote_bar'>
+				<div class = 'csenote_bar_left'><img src='$this_thumb'> $this_fname $this_lname</div>
+				<div class = 'csenote_bar_right'>
+
+				<button class='csenote_action_submit'>Add</button><button class='csenote_action_cancel'>Cancel</button></div>
+			</div>
+			</form>
+			</div>
 
 <?php
 
@@ -25,9 +41,18 @@
 
 				echo "
 				<div class='csenote'>
-					<div class='csenote_bar'>
-						<div class = 'csenote_bar_left'>". $contact['first_name'] . " " . $contact['last_name'] . "</div>
-						<div class = 'csenote_bar_right'></div>
+					<div class='csenote_bar contact_bar'>
+						<div class = 'csenote_bar_left'><h4>". $contact['first_name'] . " " . $contact['last_name'] . "</h4><h5>" . $contact['type']  . "</h5></div>
+						<div class = 'csenote_bar_right'>";
+
+						if ($_SESSION['permissions']['edit_contacts'] == '1')
+							{echo "<a href='#' class='csenote_edit'>Edit</a> ";}
+
+						if ($_SESSION['permissions']['delete_contacts'] == '1')
+							{echo "<a href='#' class='csenote_delete'>Delete</a>";}
+
+						echo "
+						</div>
 					</div>
 				</div>";
 
