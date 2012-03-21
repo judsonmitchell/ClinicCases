3ea9226e8b805c680752255edabce9e4c36577a6
Judson Mitchell <judsonmitchell@gmail.com>
2012-03-21 14:28:14 -0500

    Layout js for home page

--- html/js/Home.js
+++ html/js/Home.js
@@ -2,7 +2,39 @@
 
 
 $(document).ready(function(){
+
+	//set header widget
 	$('#home_nav').addClass('ui-toolbar ui-widget-header ui-corner-tl ui-corner-tr');
+
+	//Add navigation buttons
 	$('.home_nav_choices').buttonset();
+
+	//Add quick add button
 	$('button#quick_add').button({icons: {primary: "fff-icon-add"},text: true});
+
+	//Add navigation actions
+
+	var target = $('div#home_panel');
+
+	$('#activity_button').click(function(){
+
+		target.html('<p>Activities Here</p>');
+	});
+
+	$('#upcoming_button').click(function(){
+
+		target.html('<p>Upcoming Here</p>');
+
+	});
+
+	$('#trends_button').click(function(){
+
+		target.html('<p>Trends Here</p>');
+
+	});
+
+	//Set default view - activities
+	$('#activity_button').trigger('click');
+
 });
+
