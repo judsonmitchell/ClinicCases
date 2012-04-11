4c5e2494cbb62bb622a6be5daec66247b83baf50
Judson Mitchell <judsonmitchell@gmail.com>
2012-04-11 02:42:48 +0000

    Change file names in upgrade documents script

--- upgrade_documents.php
+++ upgrade_documents.php
@@ -4,40 +4,53 @@
 
 require('db.php');
 
-function stripallslashes($string) { 
-    	while(strchr($string,'\\')) { 
-    		$string = stripslashes($string); 
-	} 
-return $string;
-} 
+//Specify the full path to your old ClinicCases doc file
+$path_to_old_docs = '/var/www/cliniccases/docs';
 
 //Check to see if the new doc folder has been created
 if (file_exists(CC_DOC_PATH))
 {
-	echo "Good, your docs folder exists.\n";
+	echo "Good, your new docs folder exists.\n";
 }
 else
 {
-	die("Your docs folder as defined in the config file(CC_DOC_PATH) doesn't exist.  Please create it.\n");
+    die("Your new docs folder as defined in the config file(CC_DOC_PATH) doesn't
+       exist.  Please create it.\n");
 }
 
 
 //Check to see if the new doc folder is writable
 if (is_writable(CC_DOC_PATH))
 	
-	{echo "Good, Your docs folder is writable.  Let's proceed.\n";}
+	{echo "Good, Your new docs folder is writable.  Let's proceed.\n";}
 	
 	else
 
-	{die( "Your docs folder as defined in the config file (CC_DOC_PATH) is not writable. Please fix this and try again.\n"); }
+    {die( "Your new docs folder as defined in the config file (CC_DOC_PATH) is not
+   writable. Please fix this and try again.\n"); }
 
-$query = $dbh->prepare("SELECT * FROM cm_documents WHERE local_file_name != '' AND extension != 'url'");
+//Remove all backslashes from file names in the docs directory
+
+if ($handle = opendir($path_to_old_docs)) {
+
+           /* This is the correct way to loop over the directory. */
+           while (false !== ($entry = readdir($handle))) {
+                        echo "$entry\n";
+                        $old_file_name = 'docs/' . $entry;
+                        $clean_file_name = 'docs/' . str_replace("\\","",$entry);
+                        rename($old_file_name,$clean_file_name);
+           }
+}
+
+$query = $dbh->prepare("SELECT * FROM cm_documents WHERE local_file_name != ''
+  AND extension != 'url'");
 
 $query->execute();
 
 $count = $query->rowCount();
 
-echo "We will be moving $count documents from your old docs directory to ". CC_DOC_PATH . "\n";
+echo "We will be moving $count documents from your old docs directory
+    to ". CC_DOC_PATH . "\n";
 
 $docs = $query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -49,12 +62,10 @@ foreach ($docs as $doc)
 
 	$new_doc_name = $doc['id'] . '.' . $doc['extension'];
 
-	$old_doc_path = stripallslashes($doc['local_file_name']);//still not getting all slashes
-	
+    $old_doc_path = $old_file_name;
+
 	$new_doc_path = CC_DOC_PATH . '/' .  $new_doc_name;
 	
-	//exec("mv " .  escapeshellarg($doc['local_file_name']) . " " . CC_DOC_PATH  . "/" . $new_doc_name );
-
 	rename($old_doc_path,$new_doc_path);
 
 	if (!empty($doc['folder']))
@@ -62,7 +73,8 @@ foreach ($docs as $doc)
 	else
 	{$escaped_folder = '';}
 
-	$update_db = $dbh->prepare("UPDATE cm_documents SET local_file_name = :new_doc,folder = :folder	WHERE id = :id ");
+    $update_db = $dbh->prepare("UPDATE cm_documents 
+    SET local_file_name = :new_doc,folder = :folder	WHERE id = :id ");
 	
 	$data = array('new_doc' => $new_doc_name, 'id' => $doc_id, 'folder' => $escaped_folder);
 	
