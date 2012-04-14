b903a7c72b27530f39fcccd05181485d00b649da
Judson Mitchell <judsonmitchell@gmail.com>
2012-04-14 09:21:08 -0500

    Begin layout for case events

--- html/templates/interior/cases_events.php
+++ html/templates/interior/cases_events.php
@@ -1,1 +1,28 @@
-<?php echo "Events here."; ?>
+<div class="case_detail_panel_tools">
+
+	<div class="case_detail_panel_tools_left"></div>
+
+	<div class="case_detail_panel_tools_right"></div>
+
+</div>
+
+
+<?php foreach($events as $event) {extract($event);  //Geez, I just learned about php extract http://stackoverflow.com/a/8286401/49359  ?>
+
+<div class = "csenote" data-id = "<?php echo $id; ?>">
+
+	<div class = "csenote_bar">
+
+		<div class = "csenote_bar_left"><?php echo $task; ?></div>
+
+		<div class = "csenote_bar_right"></div>
+
+	</div>
+
+</div>
+
+
+
+
+
+<?php } ?>
--- lib/php/data/cases_events_load.php
+++ lib/php/data/cases_events_load.php
@@ -1,9 +1,60 @@
 <?php
 session_start();
-require('../auth/session_check.php');
+//require('../auth/session_check.php');
 require('../../../db.php');
+require('../utilities/thumbnails.php');
+require('../utilities/names.php');
 
+//$user = $_SESSION['login'];
+$user = 'jmitchell';
+//$id = $_POST['case_id'];
+$id = '1175';
 
+function get_responsibles($dbh,$event_id) //get names of all users on event
+{
+	$q = $dbh->prepare("SELECT * FROM cm_events_responsibles
+		WHERE event_id = '$event_id'");
 
+	$q->execute();
+
+	$users = $q->fetchAll(PDO::FETCH_ASSOC);
+
+	$responsibles = array();
+
+	foreach ($users as $user) {
+
+		$lastname = username_to_lastname($dbh,$user['username']);
+
+		$fullname = username_to_fullname($dbh,$user['username']);
+
+		$user_id = username_to_userid($dbh,$user['username']);
+
+		$thumb = return_thumbnail($dbh,$user['username']);
+
+		$responsibles[] = array('user_id' => $user_id,'last_name' => $lastname,
+			'full_name' => $fullname, 'thumb' => $thumb);
+	}
+
+	return $responsibles;
+}
+
+//Get all events for this case
+$get_events = $dbh->prepare("SELECT * from cm_events
+	WHERE case_id = :id ORDER BY start DESC");
+
+$data = array('id' => $id);
+
+$get_events->execute($data);
+
+$events = $get_events->fetchAll(PDO::FETCH_ASSOC);
+
+//print_r($events);die;
+
+foreach ($events as $event) {
+
+	$resps = get_responsibles($dbh,$event['id']);
+
+   	array_push($event, $resps);
+}
 
 include('../../../html/templates/interior/cases_events.php');
