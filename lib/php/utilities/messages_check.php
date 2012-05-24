cb58c68c76b43647f4c0946eade6471ed83204d0
Judson Mitchell <judsonmitchell@gmail.com>
2012-05-24 07:07:58 -0500

    Working on messages check

--- lib/php/utilities/messages_check.php
+++ lib/php/utilities/messages_check.php
@@ -15,18 +15,18 @@ $q->bindParam(1, $_SESSION['cc_session_id']);
 
 $q->execute();
 
-$last_mail_check = $q->fetch();
+$l = $q->fetch();
 
 //If there has been no mail check this session, then get time out last logout
 
-if (!$last_mail_check['last_msg_check']) {
+if (!$l['last_msg_check']) {
 	$q = $dbh->prepare("SELECT MAX(timestamp) FROM cm_logs WHERE username = :user and type = 'out'");
 
 	$user = $_SESSION['login'];
 
 	$q->execute($user);
 
-	$last_mail_check = $q->fetch();
+	$l = $q->fetch();
 
 }
 
@@ -37,7 +37,7 @@ $q->bindParam(1, $_SESSION['cc_session_id']);
 
 $q->execute();
 
-if ($last_mail_check['last_msg_check']) {
+if ($l['last_msg_check']) {
 	$q = $dbh->prepare("SELECT COUNT(id) from cm_messages WHERE `time_sent` > :last_msg_check AND (`to` LIKE :user_last OR `to` LIKE :user_middle OR `to` LIKE :user_last OR `to`LIKE :user OR `ccs` LIKE :user_last OR `ccs` LIKE :user_middle OR `ccs` LIKE :user_last or `ccs` LIKE :user)");
 
 	$user_last = '%,' . $username;
@@ -46,19 +46,26 @@ if ($last_mail_check['last_msg_check']) {
 
 	$user_first = $username . ',%';
 
-	$data = array('user' => $username,'user_last' => $user_last,'user_middle' => $user_middle,'user_first' => $user_first,'last_msg_check' => $last_mail_check['last_msg_check']);
+	$data = array('user' => $username,'user_last' => $user_last,'user_middle' => $user_middle,'user_first' => $user_first,'last_msg_check' => $l['last_msg_check']);
 
 	$q->execute($data);
 
 	$r = $q->fetch();
 
-	$number = $r['COUNT(id)'];
+	//print_r($r);
 
-	echo $number;
+	if (empty($r['COUNT(id)']))
+		{$number = '0';}
+	else
+		{$number = $r['COUNT(id)'];}
 
+	$return = array('number' => $number);
+
+	echo json_encode($return);
 
 } else {
 	//This is user's first time ever logging in
+	echo "false";
 	return false;
 }
 
