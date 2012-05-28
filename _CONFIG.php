e9afa7de01a65781cc30adef5d70361cfa952fc7
Judson Mitchell <judsonmitchell@gmail.com>
2012-05-28 09:22:04 -0500

    Update config to set case number mask

--- _CONFIG.php
+++ _CONFIG.php
@@ -2,7 +2,7 @@
 // SETS SYSTEM-WIDE CONFIG PREFERNENCES
 
 //Information to access your database
-define("CC_DBHOST","");
+define("CC_DBHOST","localhost");
 define("CC_DBUSERNAME","");
 define("CC_DBPASSWD", "");
 define("CC_DATABASE_NAME","");
@@ -18,7 +18,8 @@ define("CC_PATH","");
 //Path of directory which will store uploaded documents.  For security purposes, this directory should not be out of the webroot.  Ensure that the directory has is writeable.  E.g "/var/cc_docs" or "/home/you/private/cc_docs"
 define("CC_DOC_PATH","");
 
-//Url where your Cliniccases installation will be run, e.g. http://www.yourservername.com/yourdirectory/ .  Don't forget trailing slash!
+//Url where your Cliniccases installation will be run, e.g.
+//http://www.yourservername.com/yourdirectory/cliniccases/ .  Don't forget trailing slash!
 define("CC_BASE_URL","");
 
 //Your domain , e.g. http://www.yourserver.com
@@ -47,9 +48,16 @@ define("CC_SCHOOL_COLOR","");
 define("ALLOWED_FILE_TYPES", serialize(array('doc','docx','odt','rtf','txt','wpd','xls','ods','csv','mp3','wav','ogg','aif','aiff','mpeg','avi','mp4','mpg','mov','qt','ovg','webm','ogv','flv','bmp','jpg','jpeg','gif','png','svg','tif','tiff','zip','tar','gz','bz','pdf')));
 
 //Define constants for sending email notifications
-define("CC_EMAIL_FROM","no-reply@cliniccases.com");  //e.g. no-reply@yourserver.com
+define("CC_EMAIL_FROM","no-reply@YOURSERVER.com");  //e.g. no-reply@yourserver.com
 define("CC_EMAIL_HEADERS","From: " . CC_EMAIL_FROM . "\n" . "Reply-To: " . CC_EMAIL_FROM . "\n" . "X-Mailer: PHP/" . phpversion());
 define("CC_EMAIL_FOOTER","Please log on ClinicCases at " . CC_BASE_URL . " to view the entire message");
+
+//Define case number mask
+	//Possible values:
+	//YYYY or YY for four digit or two digit year
+	//ClinicType (derived from cm_clinic_type table) or CaseType (derived from cm_case_types table)
+	//Your mask must have at least a year value and Number, seperated by dash
+define("CC_CASE_NUMBER_MASK","YYYY-Number-ClinicType");
 
 //Magic Quotes:  CC expects magic quotes to be turned off.  This fixes that if they are not.
 
