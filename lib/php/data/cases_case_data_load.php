87c62cedcc9ddac12b893e6f819a6d0bad92ff92
Judson Mitchell <judsonmitchell@gmail.com>
2012-05-29 07:35:14 -0500

    Working on server side code for case data

--- lib/php/data/cases_case_data_load.php
+++ lib/php/data/cases_case_data_load.php
@@ -1,17 +1,39 @@
 <?php //scripts for case data tab in case detail
-session_start();
-require('../auth/session_check.php');
+// session_start();
+// require('../auth/session_check.php');
 require('../../../db.php');
 
-if (isset($_POST['id'])) {
-	$case_id = $_POST['id'];
-}
-
+// if (isset($_POST['id'])) {
+// 	$case_id = $_POST['id'];
+// }
+$case_id = '1175';
 if (isset($_POST['type'])) {
 	$type = $_POST['type'];
 }
 
-if ($type = 'new')
-	{echo "new case form";}
-else
-	{echo "Just display data";}
+//Get case data
+
+$q = $dbh->prepare("SELECT * FROM cm WHERE id = '$case_id'");
+
+$q->execute();
+
+$case_data = $q->fetch(PDO::FETCH_ASSOC);
+
+//Get columns config
+$q = $dbh->prepare("SELECT * from cm_columns");
+
+$q->execute();
+
+$columns = $q->fetchAll(PDO::FETCH_ASSOC);
+
+$data = null;
+
+foreach ($columns as $col) {
+	//push the value of the field in case_data onto $columns
+	$field =  $col['db_name'];
+	$field_value = $case_data[$field];
+	$col['value'] = $field_value;
+	$data[] = $col;
+}
+
+include '../../../html/templates/interior/cases_case_data.php';
