2fc4e882d70348fba0ded431401dada39a4f6472
Judson Mitchell <judsonmitchell@gmail.com>
2012-07-04 21:20:17 -0500

    Change to properly handle arrays

--- _UPGRADE/upgrade_from_cc_6.php
+++ _UPGRADE/upgrade_from_cc_6.php
@@ -442,7 +442,7 @@ $query->execute();
 $query = $dbh->prepare("ALTER TABLE  `cm_events` ADD  `end` DATETIME NULL AFTER  `start` ,
 ADD  `all_day` BOOLEAN NOT NULL AFTER  `end`;ALTER TABLE  `cm_events` ADD  `location` TEXT NOT NULL AFTER  `notes`;
 ALTER TABLE  `cm_events` ADD  `start_text` VARCHAR( 200 ) NOT NULL AFTER  `start` ,
-ADD  `end_text` VARCHAR( 200 ) NOT NULL AFTER  `start_text`;
+ADD  `end_text` VARCHAR( 200 ) NOT NULL AFTER  `start_text`;ALTER TABLE  `cm_events` ADD  `time_added` DATETIME NOT NULL ;
 
 ALTER TABLE  `cm_events_responsibles` ADD  `time_added` DATETIME NOT NULL;
 
