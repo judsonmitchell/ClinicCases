a94c8da653d15ed478ee0f2a08bc7022ecebb378
Judson Mitchell <judsonmitchell@gmail.com>
2013-07-29 13:16:52 -0500

    Reformat code for better readability

--- html/js/casesCaseData.js
+++ html/js/casesCaseData.js
@@ -9,7 +9,7 @@ function formatCaseData(thisPanel, type) {
     var toolsHeight = navItem.outerHeight();
     var thisPanelHeight = navItem.closest('.case_detail_nav').height();
     var documentsWindowHeight = thisPanelHeight - toolsHeight;
-    if (typeof caseNotesWindowHeight == 'undefined') {
+    if (typeof caseNotesWindowHeight === 'undefined') {
         caseNotesWindowHeight = thisPanelHeight - toolsHeight;
     }
 
@@ -42,47 +42,51 @@ function formatCaseData(thisPanel, type) {
         //Enable dynamic replacement of clinic and case codes in case number
         var cN = thisPanel.find('input[name="clinic_id"]');
         var cnVal = cN.val();
-        if (cnVal.indexOf("ClinicType") != -1) {
+        if (cnVal.indexOf('ClinicType') !== -1) {
             thisPanel.find('select[name="clinic_type"]').change(function () {
                 cN.val(cnVal.replace('ClinicType', $(this).find('option:selected').attr('data-code')));
             });
         }
 
-        if (cnVal.indexOf("CaseType") != -1) {
-            thisPanel.find('select[name="case_type"]').change(function() {
+        if (cnVal.indexOf('CaseType') !== -1) {
+            thisPanel.find('select[name="case_type"]').change(function () {
                 cN.val(cnVal.replace('CaseType', $(this).find('option:selected').attr('data-code')));
             });
         }
 
         //Add onbeforeunload event to prevent empty cases
-        $(window).bind('beforeunload', function() {
-            return "You may have unsaved changes on a case.  Please either save any changes or close the case's tab before leaving this page";
+        $(window).bind('beforeunload', function () {
+            return 'You may have unsaved changes on a case.  Please' +
+            'either save any changes or close the case\'s tab before leaving this page';
         });
 
         //Disable case number editing
-        thisPanel.find('input[name="clinic_id"]').attr('disabled', true).after('<a class="force_edit small" href="#">Let me edit this</a>');
+        thisPanel.find('input[name="clinic_id"]').attr('disabled', true)
+        .after('<a class="force_edit small" href="#">Let me edit this</a>');
 
-        thisPanel.find('a.force_edit').click(function(event) {
+        thisPanel.find('a.force_edit').click(function (event) {
             event.preventDefault();
-            var dialogWin = $('<div class="dialog-casenote-delete" title="Are you sure?"><p>ClinicCases automatically assigns the next available case number.  If your case number contains "CaseType" or "ClinicType", these values will be replaced when you change those fields below.</p><br /><p>Manually editing a case number may have undesirable results. Are you sure?</p></div>').dialog({
+            var dialogWin = $('<div class="dialog-casenote-delete" title="Are you sure?"><p>' +
+            'ClinicCases automatically assigns the next available case number.  If your' +
+            'case number contains "CaseType" or "ClinicType", these values will be ' +
+            'replaced when you change those fields below.</p><br /><p>Manually editing' +
+            'a case number may have undesirable results. Are you sure?</p></div>').dialog({
                 autoOpen: false,
                 resizable: false,
                 modal: true,
                 buttons: {
-                    "Yes": function() {
+                    'Yes': function () {
                         $('input[name="clinic_id"]').attr('disabled', false).focus();
                         thisPanel.find('a.force_edit').remove();
-                        $(this).dialog("destroy");
+                        $(this).dialog('destroy');
                     },
-                    "No": function() {
-                        $(this).dialog("destroy");
+                    'No': function () {
+                        $(this).dialog('destroy');
                     }
                 }
             });
 
             $(dialogWin).dialog('open');
-
-
         });
 
         //Add case re-opening feature
@@ -91,26 +95,25 @@ function formatCaseData(thisPanel, type) {
 
             thisPanel.find('a.force_reopen').click(function (event) {
                 event.preventDefault();
-                var dialogWin = $('<div class="dialog-casenote-delete" title="Are you sure?"><p>This will re-open the case. Are you sure?</p></div>').dialog({
+                var dialogWin = $('<div class="dialog-casenote-delete" title="Are you sure?">' +
+                '<p>This will re-open the case. Are you sure?</p></div>').dialog({
                     autoOpen: false,
                     resizable: false,
                     modal: true,
                     buttons: {
-                        "Yes": function() {
-                            $('input[name="date_close"]').datepicker("setDate", null).next().html('');
-                            $('select[name="dispo"]').val('').trigger("liszt:updated");
+                        'Yes': function () {
+                            $('input[name="date_close"]').datepicker('setDate', null).next().html('');
+                            $('select[name="dispo"]').val('').trigger('liszt:updated');
                             thisPanel.find('a.force_reopen').remove();
-                            $(this).dialog("destroy");
+                            $(this).dialog('destroy');
                         },
-                        "No": function() {
-                            $(this).dialog("destroy");
+                        'No': function () {
+                            $(this).dialog('destroy');
                         }
                     }
                 });
 
                 $(dialogWin).dialog('open');
-
-
             });
         }
         //Add chosen to selects
@@ -123,21 +126,20 @@ function formatCaseData(thisPanel, type) {
         thisPanel.find('span.multi-text').not('label + span.multi-text').css({'margin-left': '190px'});
 
         //Add link to trigger a new dual field
-        thisPanel.find('p').has('span.dual_input').each(function() {
-
+        thisPanel.find('p').has('span.dual_input').each(function () {
             $(this).find('span.dual_input').last().after('<a class="add_another small" href="#">Add another</a>');
         });
 
         //Add link to a new multi-text field
-        thisPanel.find('p').has('span.multi-text').each(function() {
+        thisPanel.find('p').has('span.multi-text').each(function () {
             $(this).find('span.multi-text').last().after('<a class="add_another small" href="#">Add another</a>');
         });
 
         //Add datepickers
-        thisPanel.find('input.date_field').each(function() {
+        thisPanel.find('input.date_field').each(function () {
             var b = $.datepicker.parseDate('yy-mm-dd', $(this).val());
             var buttonVal = $.datepicker.formatDate('mm/dd/yy', b);
-            $(this).datepicker({dateFormat: 'yy-mm-dd',showOn: 'button',buttonText: buttonVal,onSelect: function(dateText, inst) {
+            $(this).datepicker({dateFormat: 'yy-mm-dd', showOn: 'button', buttonText: buttonVal, onSelect: function (dateText) {
                     var c = $.datepicker.parseDate('yy-mm-dd', dateText);
                     var displayDate = $.datepicker.formatDate('mm/dd/yy', c);
                     $(this).next().html(displayDate);
@@ -153,7 +155,7 @@ function formatCaseData(thisPanel, type) {
         //Change name on tab when user enters last name
         thisPanel.find('input[name="first_name"]').focus();
 
-        $('input[name = "last_name"]').keyup(function() {
+        $('input[name = "last_name"]').keyup(function () {
             var fname = thisPanel.find('input[name="first_name"]').val();
             $(this).closest('#case_detail_tab_row')
             .find('li.ui-state-active').find('a').html($(this).val() + ', ' + fname);
@@ -163,12 +165,11 @@ function formatCaseData(thisPanel, type) {
         });
 
         //If there is no last name, put the organization name on the tab
-        $('input[name = "organization"]').keyup(function (event) {
+        $('input[name = "organization"]').keyup(function () {
 
-            lnameVal = $(this).closest('form').find('input[name="last_name"]').val();
+            var lnameVal = $(this).closest('form').find('input[name="last_name"]').val();
 
-            if (lnameVal === '')
-            {
+            if (lnameVal === '') {
                 $(this).closest('#case_detail_tab_row')
                 .find('li.ui-state-active').find('a').html($(this).val());
 
@@ -176,14 +177,11 @@ function formatCaseData(thisPanel, type) {
                 $(this).closest('#case_detail_tab_row').find('div.case_title').html('<h2>' + $(this).val() + '</h2>');
             }
         });
-    }
-
-    else  //display case data
-    {
+    } else {  //display case data
+    
         //format buttons
-        thisPanel.find('button.case_data_edit').button({icons: {primary: "fff-icon-page-edit"},text: true});
-
-        thisPanel.find('button.case_data_print').button({icons: {primary: "fff-icon-printer"},text: true});
+        thisPanel.find('button.case_data_edit').button({icons: {primary: 'fff-icon-page-edit'}, text: true});
+        thisPanel.find('button.case_data_print').button({icons: {primary: 'fff-icon-printer'}, text: true});
 
         //remove the id
         thisPanel.find('div.id_display').remove();
@@ -192,7 +190,7 @@ function formatCaseData(thisPanel, type) {
 
 
 //User clicks on Case Data in left-side navigation
-$('.case_detail_nav #item2').live('click', function() {
+$('.case_detail_nav #item2').live('click', function () {
 
     var thisPanel = $(this).closest('.case_detail_nav').siblings('.case_detail_panel');
     var caseId = $(this).closest('.case_detail_nav').siblings('.case_detail_panel').data('CaseNumber');
@@ -204,7 +202,7 @@ $('.case_detail_nav #item2').live('click', function() {
         type = 'display';
     }
 
-    thisPanel.load('lib/php/data/cases_case_data_load.php', {'id': caseId,'type': type}, function(data) {
+    thisPanel.load('lib/php/data/cases_case_data_load.php', {'id': caseId,'type': type}, function (data) {
         formatCaseData(thisPanel, type);
     });
 });
@@ -216,134 +214,111 @@ $('button.case_data_edit').live('click', function () {
 
     var thisPanel = $(this).closest('.case_detail_panel');
     var thisCaseId = thisPanel.data('CaseNumber');
-    thisPanel.load('lib/php/data/cases_case_data_load.php', {'id': thisCaseId,'type': 'edit'}, function() {
+    thisPanel.load('lib/php/data/cases_case_data_load.php', {'id': thisCaseId,'type': 'edit'}, function () {
         formatCaseData(thisPanel, 'edit');
     });
 });
 
 //Submit the form
-$('button.case_modify_submit').live('click', function(event) {
+$('button.case_modify_submit').live('click', function (event) {
 
     event.preventDefault();
-
     var resultTarget = $(this).closest('.case_detail_panel');
     var thisCaseId = resultTarget.data('CaseNumber');
+    var actionType;
 
     //Server-side script does different things depending on whether
     //this is a new case which is just being opened or this is an
     //existing case which is being edited.  So, set variable:
-    if ($(this).hasClass('update_new_case'))
-    {
+    if ($(this).hasClass('update_new_case')) {
         actionType = 'update_new_case';
-    }
-    else
-    {
+    } else {
         actionType = 'edit';
     }
 
-    $(window).unbind("beforeunload");
+    $(window).unbind('beforeunload');
 
     var formVals = $(this).closest('form');
 
     //enable clinic_id field or else serializeArray won't pick up value
     formVals.find('input[name="clinic_id"]').attr({'disabled': false});
-
     var errString = newCaseValidate(formVals);
-
     var formValsArray = formVals.serializeArray();
 
 
     //notify user or errors or submit form
-    if (errString.length)
-    {
+    if (errString.length) {
         notify(errString, true);
 
         //Reapply onbeforeunload event to prevent empty cases
-        $(window).bind('beforeunload', function() {
-            return "You may have unsaved changes on a case.  Please either save any changes or close the case's tab before leaving this page";
+        $(window).bind('beforeunload', function () {
+            return 'You may have unsaved changes on a case.  Please either save' +
+            'any changes or close the case\'s tab before leaving this page';
         });
 
-        $('input.ui-state-error').focus(function() {
+        $('input.ui-state-error').focus(function () {
             $(this).removeClass('ui-state-error');
         });
         return false;
-    }
-    else
-    {
+    } else {
         //Reserialize form, taking out dual_inputs and multi-texts.  They are
         //dealt with seperately.
 
-        formValsOk = $(this).closest('form').find(':not(span.dual_input input, span.dual_input select, span.multi-text input)').serializeArray();
+        var formValsOk = $(this).closest('form')
+        .find(':not(span.dual_input input, span.dual_input select, span.multi-text input)')
+        .serializeArray();
 
-        formValsOk.push({'name': 'action','value': actionType});
+        formValsOk.push({'name': 'action', 'value': actionType});
 
         //Extract values from all multi-text fields
-        formVals.find('p').has('.multi-text').each(function() {
+        formVals.find('p').has('.multi-text').each(function () {
 
             var dataObj = {};
-
             var dataName = $(this).find('input').attr('name');
-
-            $(this).find('span.multi-text').each(function() {
-
+            $(this).find('span.multi-text').each(function () {
                 var dataValue = $(this).find('input').val();
-
-                if (dataValue.length > 0)
-                {
+                if (dataValue.length > 0) {
                     dataObj[dataValue] = 'name';
                 }
             });
 
-            if (!$.isEmptyObject(dataObj))
-            {
-                dataJson = JSON.stringify(dataObj);
-
-                formValsOk.push({'name': dataName,'value': dataJson});
+            if (!$.isEmptyObject(dataObj)) {
+                var dataJson = JSON.stringify(dataObj);
+                formValsOk.push({'name': dataName, 'value': dataJson});
             }
 
         });
 
         //Extract values from all dual inputs
-        formVals.find('p').has('.dual_input').each(function() {
+        formVals.find('p').has('.dual_input').each(function () {
 
             var dataObj = {};
-
             var dataName = $(this).find('input:last').attr('name');
-
-            $(this).find('span.dual_input').each(function() {
-
+            $(this).find('span.dual_input').each(function () {
                 var dataType = $(this).find('select.dual').val();
                 var dataValue = $(this).find('input:last').val();
-                if (dataValue.length > 0)
-                {
+                if (dataValue.length > 0) {
                     dataObj[dataValue] = dataType;
                 }
-
             });
 
-            if (!$.isEmptyObject(dataObj))
-            {
-                dataJson = JSON.stringify(dataObj);
-
-                formValsOk.push({'name': dataName,'value': dataJson});
+            if (!$.isEmptyObject(dataObj)) {
+                var dataJson = JSON.stringify(dataObj);
+                formValsOk.push({'name': dataName, 'value': dataJson});
             }
-
         });
 
         //Submit to server
-        $.post('lib/php/data/cases_case_data_process.php', formValsOk, function(data) {
+        $.post('lib/php/data/cases_case_data_process.php', formValsOk, function (data) {
             var serverResponse = $.parseJSON(data);
 
-            if (serverResponse.error === true)
-            {
+            if (serverResponse.error === true) {
                 notify(serverResponse.message, true);
-            }
-            else
-            {
+            } else {
                 notify(serverResponse.message);
                 $('#case_detail_tab_row').find('li.ui-state-active').removeClass('ui-state-highlight ui-state-error new_case');
 
-                resultTarget.load('lib/php/data/cases_case_data_load.php', {'id': thisCaseId,'type': 'display'}, function(data) {
+                resultTarget.load('lib/php/data/cases_case_data_load.php', {'id': thisCaseId, 'type': 'display'}, function () {
                     formatCaseData(resultTarget, 'display');
                 });
 
@@ -362,7 +337,7 @@ $('button.case_cancel_submit').live('click', function (event) {
     event.preventDefault();
     var thisPanel = $(this).closest('.case_detail_panel');
     var caseId = $(this).closest('.case_detail_panel').data('CaseNumber');
-    var type;  
+    var type;
 
     if ($(this).hasClass('cancel_new_case')) {
         var dialogWin = $('<div class="dialog-casenote-delete" title="Are you sure?"><p>' +
@@ -371,7 +346,7 @@ $('button.case_cancel_submit').live('click', function (event) {
             resizable: false,
             modal: true,
             buttons: {
-                "Yes": function() {
+                'Yes': function () {
                     $.post('lib/php/data/cases_case_data_process.php', {id: caseId, action: 'delete'}, function (data) {
                         var serverResponse = $.parseJSON(data);
                         if (!serverResponse.error) {
@@ -380,34 +355,34 @@ $('button.case_cancel_submit').live('click', function (event) {
                         }
                         notify(serverResponse.message);
                     });
-                    $(this).dialog("destroy");
+                    $(this).dialog('destroy');
                 },
-                "No": function() {
-                    $(this).dialog("destroy");
+                'No': function () {
+                    $(this).dialog('destroy');
                 }
             }
         });
 
         $(dialogWin).dialog('open');
     } else {
-        var thisPanel = $(this).closest('.case_detail_panel');
-        var caseId = $(this).closest('.case_detail_panel').data('CaseNumber');
         type = 'display';
-        thisPanel.load('lib/php/data/cases_case_data_load.php', {'id': caseId,'type': type}, function (data) {
+        thisPanel.load('lib/php/data/cases_case_data_load.php', {'id': caseId, 'type': type}, function () {
             formatCaseData(thisPanel, type);
         });
     }
 
-    $(window).unbind("beforeunload");
+    $(window).unbind('beforeunload');
 });
 
 //Listen for print
-$('button.case_data_print').live('click', function() {
-    elPrint($(this).closest('div.case_detail_panel_tools').siblings('div.case_detail_panel_casenotes'), 'Case Data: ' + $(this).closest('.case_detail_panel').siblings('.case_detail_bar').find('.case_title').text());
+$('button.case_data_print').live('click', function () {
+    elPrint($(this).closest('div.case_detail_panel_tools')
+    .siblings('div.case_detail_panel_casenotes'), 'Case Data: ' +
+    $(this).closest('.case_detail_panel').siblings('.case_detail_bar').find('.case_title').text());
 });
 
 //Add another multi-text or dual
-$('a.add_another').live('click', function(event) {
+$('a.add_another').live('click', function (event) {
     event.preventDefault();
     var newField = $(this).prev('span').clone();
     newField.find('input').val('');
--- html/js/notifyUser.js
+++ html/js/notifyUser.js
@@ -1,22 +1,16 @@
 //js functions to notify user.
 
+function notify(str, wait) {
+    $('#notifications').html(str).css({'opacity': '.8'});
+    $('#notifications').addClass('ui-corner-all').show();
 
-function notify(str,wait)
-	{
-		$("#notifications").html(str).css({'opacity':'.8'});
-		$("#notifications").addClass('ui-corner-all').show();
-
-		if (wait === true)
-		{
-
-			$('#notifications').append('<p><a href="">Dismiss</a></p>');
-			$('#notifications a').click(function(event){
-				event.preventDefault();
-				$('#notifications').fadeOut();
-				});
-		}
-		else
-		{
-			$('#notifications').delay(2000).fadeOut();
-		}
-	}
+    if (wait === true) {
+        $('#notifications').append('<p><a href="">Dismiss</a></p>');
+        $('#notifications a').click(function (event) {
+            event.preventDefault();
+            $('#notifications').fadeOut();
+        });
+    } else {
+        $('#notifications').delay(2000).fadeOut();
+    }
+}
