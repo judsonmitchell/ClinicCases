d03a909d94805dd63f442948bd8b14b0633b6280
Judson Mitchell <judsonmitchell@gmail.com>
2015-06-11 06:04:26 -0500

    Remove PDFJS css

--- html/templates/Header.php
+++ html/templates/Header.php
@@ -34,7 +34,6 @@
 	<link rel="stylesheet" href="lib/jqueryui/css/custom-theme/jquery-ui-1.8.9.custom.css" type="text/css" />
 	<link type="text/css" href="html/css/fff.icon.core.css" rel="stylesheet"/>
 	<link type="text/css" href="html/css/fff.icon.icons.css" rel="stylesheet"/>
-	<link type="text/css" href="lib/javascripts/pdfjs-dist/web/pdf_viewer.css" rel="stylesheet"/>
 	<link rel="shortcut icon" type="image/x-icon" href="html/images/favicon.ico" />
 	<script src="lib/jqueryui/js/jquery-1.4.4.min.js" type="text/javascript"></script>
 	<script src="lib/jqueryui/js/jquery-ui-1.8.9.custom.min.js" type="text/javascript"></script>
