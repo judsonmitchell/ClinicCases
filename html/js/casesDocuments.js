057c178884d81b0e56299c39ba25b1320be80133
Judson Mitchell <judsonmitchell@gmail.com>
2015-06-11 10:25:37 -0500

    Revert to download method for older browsers

--- html/js/casesDocuments.js
+++ html/js/casesDocuments.js
@@ -287,82 +287,32 @@ function openItem(el, itemId, docType, caseId, path, pathDisplay) {
             serverResponse.ccd_content, serverResponse.ccd_id,serverResponse.ccd_owner,serverResponse.ccd_locked);
         });
     } else if ($(el).hasClass('pdf')){
-        $('#pdf-viewer').show();
-        $('#frme').attr('src', 'lib/javascripts/pdfjs/web/viewer.html?item_id=' + itemId);
+        if (Object.create){ //informal browser check for ie8
+            $('#pdf-viewer').show();
+            $('#frme').attr('src', 'lib/javascripts/pdfjs/web/viewer.html?item_id=' + itemId);
 
-        //Add listener to close pdf viewer
-        $('#pdf-viewer').click(function(){
-            $('#frme').attr('src','');
-            $(this).hide();
-        });
-        //$.post( 'lib/php/data/cases_documents_process.php', {
-            //'action': 'open',
-            //'item_id': itemId,
-            //'doc_type': 'pdf'
-            //},
-            //function(data) {
-                //PDFJS.getDocument(data).then(function (data){
-                    //alert(data.numPages);
-                //});
-        //})
-        //var xhr = new XMLHttpRequest();
-        //var url = 'lib/php/data/cases_documents_process.php';
-        //var params = 'action=open&item_id=' + itemId + '&doc_type=pdf';
-        //xhr.responseType = 'arraybuffer';
-        //xhr.open('POST', url, true);
-
-        ////Send the proper header information along with the request
-        //xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-        ////xhr.setRequestHeader('Content-length', params.length);
-        ////xhr.setRequestHeader('Connection', 'close');
-
-        //xhr.onreadystatechange = function() {//Call a function when the state changes.
-            //if(xhr.readyState === 4 && xhr.status === 200) {
-                //var responseArray = new Uint8Array(xhr.response);
-                //PDFJS.getDocument(responseArray).then(function (pdfDocument) {
-                   //// return pdfDocument.getPage(1).then(function (pdfPage) {
-                   ////     var pdfPageView = new PDFJS.PDFPageView({
-                   ////         container: document.getElementById('pdf-viewer'),
-                   ////         id: 1,
-                   ////         scale: SCALE,
-                   ////         defaultViewport: pdfPage.getViewport(SCALE),
-                   ////         // We can enable text/annotations layers, if needed
-                   ////         textLayerFactory: new PDFJS.DefaultTextLayerFactory(),
-                   ////         annotationsLayerFactory: new PDFJS.DefaultAnnotationsLayerFactory()
-                   ////     });
-                   ////     // Associates the actual page with the view, and drawing it
-                   ////     pdfPageView.setPdfPage(pdfPage);
-                   ////     return pdfPageView.draw();
-                   //// });
-                 ////
-                    //// Prepare canvas using PDF page dimensions
-                    ////
-                    //pdfDocument.getPage(1).then(function (page){
-                        //var scale = 1.0;
-                        //var viewport = page.getViewport(scale);
-                        //var canvas = document.getElementById('the-canvas');
-                        //var context = canvas.getContext('2d');
-                        //canvas.height = viewport.height;
-                        //canvas.width = viewport.width;
-                        ////
-                        //// Render PDF page into canvas context
-                        ////
-                        //var renderContext = {
-                            //canvasContext: context,
-                            //viewport: viewport
-                        //};
-                        //page.render(renderContext);
-                    //});
-                //});
-            //}
-        //};
-        //xhr.send(params);
+            //Add listener to close pdf viewer
+            $('#pdf-viewer').click(function(){
+                $('#frme').attr('src','');
+                $(this).hide();
+            });
+        } else {
+            $.download('lib/php/data/cases_documents_process.php', {
+                'item_id': itemId,
+                'action': 'open',
+                'doc_type': docType
+            });
+        }
     } else {
         if ( $(el).hasClass('.ui-draggable-dragging') ) {
             return;
         }
 
-        $.download('lib/php/data/cases_documents_process.php', {'item_id': itemId,'action': 'open','doc_type': docType});
+        $.download('lib/php/data/cases_documents_process.php', {
+            'item_id': itemId,
+            'action': 'open',
+            'doc_type': docType
+        });
     }
 
 }
