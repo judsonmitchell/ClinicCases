a72d798d296a90bde2e0cc117b3fff178f303c8f
Judson Mitchell <judsonmitchell@gmail.com>
2015-06-14 08:50:11 -0500

    Code clean up, linting

--- html/js/casesConflicts.js
+++ html/js/casesConflicts.js
@@ -1,7 +1,8 @@
 //Scripts for conflicts
 
-$('.case_detail_nav #item7').live('click', function() {
+/* global elPrint */
 
+$('.case_detail_nav #item7').live('click', function() {
 	var thisPanel = $(this).closest('.case_detail_nav').siblings('.case_detail_panel');
     var caseId = $(this).closest('.case_detail_nav').siblings('.case_detail_panel').data('CaseNumber');
 
@@ -10,8 +11,9 @@ $('.case_detail_nav #item7').live('click', function() {
     var thisPanelHeight = $(this).closest('.case_detail_nav').height();
     var conflictsWindowHeight = thisPanelHeight - toolsHeight;
 
-    thisPanel.load('lib/php/data/cases_conflicts_load.php',{'case_id':caseId},function(data){
-
+    thisPanel.load('lib/php/data/cases_conflicts_load.php',{
+        'case_id':caseId
+    },function(data){
 		//Set css
         $('div.case_detail_panel_tools').css({'height': toolsHeight});
         $('div.case_detail_panel_casenotes').css({'height': conflictsWindowHeight});
@@ -21,20 +23,23 @@ $('.case_detail_nav #item7').live('click', function() {
         //Apply shadow on scroll
         $(this).children('.case_detail_panel_casenotes').bind('scroll', function() {
             var scrollAmount = $(this).scrollTop();
-            if (scrollAmount === 0 && $(this).hasClass('csenote_shadow'))
-            {
+            if (scrollAmount === 0 && $(this).hasClass('csenote_shadow')) {
                 $(this).removeClass('csenote_shadow');
-            }
-            else
-            {
+            } else {
                 $(this).addClass('csenote_shadow');
             }
         });
 
-		$('button.conflicts_print').button({icons: {primary: "fff-icon-printer"},text: true}).click(function() {
-            elPrint($(this).closest('div.case_detail_panel_tools').siblings('div.case_detail_panel_casenotes'),'Conflicts: ' + $(this).closest('.case_detail_panel').siblings('.case_detail_bar').find('.case_title').text());
-            });
-
-
+		$('button.conflicts_print').button({
+            icons: {primary: 'fff-icon-printer'},
+            text: true
+        }).click(function() {
+            elPrint($(this).closest('div.case_detail_panel_tools')
+                .siblings('div.case_detail_panel_casenotes'),'Conflicts: ' + $(this).closest('.case_detail_panel')
+                .siblings('.case_detail_bar')
+                .find('.case_title')
+                .text()
+            );
         });
+    });
 });
