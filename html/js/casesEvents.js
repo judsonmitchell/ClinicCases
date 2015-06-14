dd92bd067a40299c23d6c227116c00eab8695972
Judson Mitchell <judsonmitchell@gmail.com>
2015-06-14 07:22:51 -0500

    Code clean up, linting

--- html/js/casesEvents.js
+++ html/js/casesEvents.js
@@ -2,9 +2,10 @@
 // Scripts for events panel on cases
 //
 
+/* global notify, elPrint, validEvent */
+
 //User clicks on Events in left-side navigation
 $('.case_detail_nav #item4').live('click', function() {
-
 	var thisPanel = $(this).closest('.case_detail_nav').siblings('.case_detail_panel');
     var caseId = $(this).closest('.case_detail_nav')
         .siblings('.case_detail_panel')
@@ -15,8 +16,9 @@ $('.case_detail_nav #item4').live('click', function() {
     var thisPanelHeight = $(this).closest('.case_detail_nav').height();
     var eventsWindowHeight = thisPanelHeight - toolsHeight;
 
-    thisPanel.load('lib/php/data/cases_events_load.php', {'case_id': caseId}, function() {
-
+    thisPanel.load('lib/php/data/cases_events_load.php', {
+        'case_id': caseId
+    }, function() {
         //Set css
         $('div.case_detail_panel_tools').css({'height': toolsHeight});
         $('div.case_detail_panel_casenotes').css({'height': eventsWindowHeight});
@@ -24,8 +26,8 @@ $('.case_detail_nav #item4').live('click', function() {
         $('div.case_detail_panel_tools_right').css({'width': '70%'});
 
         //Set buttons
-        $('button.new_event').button({icons: {primary: "fff-icon-calendar-add"},text: true})
-            .next().button({icons: {primary: "fff-icon-printer"},text: true});
+        $('button.new_event').button({icons: {primary: 'fff-icon-calendar-add'},text: true})
+            .next().button({icons: {primary: 'fff-icon-printer'},text: true});
 
         //Round Corners
         $('div.csenote').addClass('ui-corner-all');
@@ -33,23 +35,17 @@ $('.case_detail_nav #item4').live('click', function() {
         //Apply shadow on scroll
         $(this).children('.case_detail_panel_casenotes').bind('scroll', function() {
             var scrollAmount = $(this).scrollTop();
-            if (scrollAmount === 0 && $(this).hasClass('csenote_shadow'))
-            {
+            if (scrollAmount === 0 && $(this).hasClass('csenote_shadow')) {
                 $(this).removeClass('csenote_shadow');
-            }
-            else
-            {
+            } else {
                 $(this).addClass('csenote_shadow');
             }
         });
-
     });
-
 });
 
 //Add event
 $('.case_detail_panel_tools_right button.new_event').live('click', function() {
-
     //make sure events are scrolled to top
     $(this).closest('.case_detail_panel_tools')
         .siblings('.case_detail_panel_casenotes').scrollTop(0);
@@ -98,21 +94,20 @@ $('.case_detail_panel_tools_right button.new_event').live('click', function() {
         event.preventDefault();
         //reset form
         addEventWidget.find('form')[0].reset();
-        addEventWidget.find('span.event_name_live').html("New Event");
+        addEventWidget.find('span.event_name_live').html('New Event');
 
         addEventWidget.find('select option')
-            .filter(function() {return this.text == 'You';})
+            .filter(function() {return this.text === 'You';})
             .attr('selected',true);
 
         //refresh Chosen
-        addEventWidget.find('select').trigger("liszt:updated");
+        addEventWidget.find('select').trigger('liszt:updated');
 
         //reset opacity of other case notes
         $(this).closest('.case_detail_panel_casenotes')
             .find('.event').css({'opacity': '1'});
         //hide the widget
         $(this).closest('.new_event').hide();
-
     });
 
     //User adds new event
@@ -121,7 +116,6 @@ $('.case_detail_panel_tools_right button.new_event').live('click', function() {
         .click(function(event) {
 
         event.preventDefault();
-
         //Do validation
         var eventForm = $(this).closest('form');
         var eventVals = eventForm.serializeArray();
@@ -133,17 +127,12 @@ $('.case_detail_panel_tools_right button.new_event').live('click', function() {
         var errString = validEvent(eventVals);
 
         //notify user or errors or submit form
-        if (errString.length)
-        {
+        if (errString.length) {
             notify(errString, true);
-        }
-        else
-        {
+        } else {
             //submit the form
             var caseId = $(this).closest('.case_detail_panel').data('CaseNumber');
-
             var target = $(this).closest('.case_detail_panel_casenotes');
-
             $.post('lib/php/data/cases_events_process.php', {
                 'task': eventForm.find('input[name = "task"]').val(),
                 'where': eventForm.find('input[name = "where"]').val(),
@@ -154,20 +143,18 @@ $('.case_detail_panel_tools_right button.new_event').live('click', function() {
                 'responsibles': resps,
                 'action': 'add',
                 'case_id': caseId
-                }, function(data) {
-
+            }, function(data) {
                     var serverResponse = $.parseJSON(data);
-                    if (serverResponse.error === true)
-                                {notify(serverResponse.message,true);}
-                        else
-                        {
-                            notify(serverResponse.message);
-                            target.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {'case_id': caseId});
-                        }
-
+                    if (serverResponse.error === true) {
+                        notify(serverResponse.message,true);
+                    } else {
+                        notify(serverResponse.message);
+                        target.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {
+                            'case_id': caseId
+                        });
+                    }
                 });
         }
-
     });
 
 });
@@ -179,69 +166,50 @@ $('input[name="task"]').live('keyup', function() {
 
 //handle search
 $('input.events_search').live('focusin', function() {
-
     $(this).val('');
     $(this).css({'color': 'black'});
     $(this).next('.events_search_clear').show();
 });
 
 $('input.events_search').live('keyup', function() {
-
     var resultTarget = $(this).closest('div.case_detail_panel_tools').next();
-
     var search = $(this).val();
-
     var caseId = $(this).closest('.case_detail_panel').data('CaseNumber');
 
-    //resultTarget.unbind('scroll');
-
-    resultTarget.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {'case_id': caseId,'q': search}, function() {
-
+    resultTarget.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {
+        'case_id': caseId,
+        'q': search
+    }, function() {
         resultTarget.scrollTop(0);
+        if (search.length) {
+            resultTarget.highlight(search);
+        }
 
-        if (search.length)
-            {
-                resultTarget.highlight(search);
-            }
-
-        if (resultTarget.hasClass('csenote_shadow'))
-        {
+        if (resultTarget.hasClass('csenote_shadow')) {
             resultTarget.removeClass('csenote_shadow');
         }
 
         $('div.event').addClass('ui-corner-all');
-
         resultTarget.bind('scroll.search', function() {
-            if ($(this).scrollTop() > 0)
-            {
+            if ($(this).scrollTop() > 0) {
                 $(this).addClass('csenote_shadow');
-            }
-            else
-            {
+            } else {
                 $(this).removeClass('csenote_shadow');
             }
         });
-
     });
-
 });
 
 $('.events_search_clear').live('click', function() {
-
     $(this).prev().val('Search Events');
-
     $(this).prev().css({'color': '#AAA'});
-
     var resultTarget = $(this).closest('div.case_detail_panel_tools').next();
-
     var caseId = $(this).closest('.case_detail_panel').data('CaseNumber');
-
-    resultTarget.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {'case_id': caseId}, function() {
-
+    resultTarget.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {
+        'case_id': caseId
+    }, function() {
         resultTarget.scrollTop(0);
-
         $('div.event').addClass('ui-corner-all');
-
     });
 
     $(this).hide();
@@ -250,53 +218,53 @@ $('.events_search_clear').live('click', function() {
 //Delete Event
 $('a.event_delete').live('click', function(event) {
     event.preventDefault();
-
     var thisEvent = $(this).closest('.event');
     var thisEventId = thisEvent.attr('data-id');
-    var dialogWin = $('<div class=".dialog-casenote-delete" title="Delete this Event?">This event will be permanently deleted.  Are you sure?</div>').dialog({
+    var dialogWin = $('<div class=".dialog-casenote-delete" title="Delete this Event?">This event' +
+    'will be permanently deleted.  Are you sure?</div>')
+    .dialog({
         autoOpen: false,
         resizable: false,
         modal: true,
         buttons: {
-            "Yes": function() {
-                $.post('lib/php/data/cases_events_process.php', {'action': 'delete','event_id': thisEventId}, function(data) {
-                        var serverResponse = $.parseJSON(data);
-                        if (serverResponse.error === true)
-                            {notify(serverResponse.message,true);}
-                        else
-                        {
-                            notify(serverResponse.message);
-                            thisEvent.remove();
-                        }
-
+            'Yes': function() {
+                $.post('lib/php/data/cases_events_process.php', {
+                    'action': 'delete',
+                    'event_id': thisEventId
+                }, function(data) {
+                    var serverResponse = $.parseJSON(data);
+                    if (serverResponse.error === true) {
+                        notify(serverResponse.message,true);
+                    } else {
+                        notify(serverResponse.message);
+                        thisEvent.remove();
+                    }
                 });
-
-                $(this).dialog("destroy");
+                $(this).dialog('destroy');
             },
-            "No": function() {
-                $(this).dialog("destroy");
+            'No': function() {
+                $(this).dialog('destroy');
             }
         }
     });
 
     $(dialogWin).dialog('open');
-
 });
 
 //Print displayed events
 $('.case_detail_panel_tools_right button.events_print').live('click', function() {
-   elPrint($(this).closest('div.case_detail_panel_tools').siblings('div.case_detail_panel_casenotes'),'Case Data: ' + $(this).closest('.case_detail_panel').siblings('.case_detail_bar').find('.case_title').text());
-    });
+    elPrint($(this).closest('div.case_detail_panel_tools')
+        .siblings('div.case_detail_panel_casenotes'),'Case Data: ' + $(this).closest('.case_detail_panel')
+        .siblings('.case_detail_bar')
+        .find('.case_title')
+        .text());
+});
 
 //edit event
-
 $('a.event_edit').live('click', function(event) {
-
     event.preventDefault();
-
     //test to see if there is another contact being edited.  If so , return false
-    if ($(this).closest('.case_detail_panel_casenotes').find('.event_edit_submit').length)
-    {
+    if ($(this).closest('.case_detail_panel_casenotes').find('.event_edit_submit').length) {
         notify('Only one event can be edited at a time', true);
         return false;
     }
@@ -304,6 +272,7 @@ $('a.event_edit').live('click', function(event) {
     //define event to be edited
     var thisEvent = $(this).closest('.event');
     var eventId = thisEvent.attr('data-id');
+    var allDayVal;
 
     //get form values
     var taskVal = thisEvent.find('span.event_task_title').html();
@@ -311,15 +280,19 @@ $('a.event_edit').live('click', function(event) {
     var endVal = thisEvent.find('span.event_end').html();
     var locationVal = thisEvent.find('span.event_location').html();
     var notesVal = thisEvent.find('span.event_notes').html();
-    if (thisEvent.find('span.event_all_day').length)
-        {allDayVal = 'on';}
+    if (thisEvent.find('span.event_all_day').length) {
+        allDayVal = 'on';
+    }
     var usersArray = [];
     thisEvent.find('span.user_identifier').each(function(){
         usersArray.push($(this).attr('data'));
     });
 
     //define the dummy version of the event used for editing
-    var editEvent = $(this).closest('div.event').siblings('div.new_event').clone().addClass('event_edit');
+    var editEvent = $(this).closest('div.event')
+        .siblings('div.new_event')
+        .clone()
+        .addClass('event_edit');
     thisEvent.after(editEvent);
 
     //get position of contact so that it will displayed correctly after edit
@@ -355,9 +328,10 @@ $('a.event_edit').live('click', function(event) {
 
     //set css
     editEvent.find('.csenote_bar').css({'background-color': '#FEBBBB'});
-    editEvent.find('button.event_action_submit').html('Done').addClass('event_edit_submit').removeClass('event_action_submit');
+    editEvent.find('button.event_action_submit').html('Done')
+        .addClass('event_edit_submit')
+        .removeClass('event_action_submit');
     editEvent.find('button.event_action_cancel').addClass('event_edit_cancel').removeClass('event_action_cancel');
-
     editEvent.show();
     thisEvent.hide();
 
@@ -371,7 +345,6 @@ $('a.event_edit').live('click', function(event) {
     //user submits edits
     editEvent.find('button.event_edit_submit').click(function(event) {
         event.preventDefault();
-
         //Do validation
         var eventForm = $(this).closest('form');
         var eventVals = eventForm.serializeArray();
@@ -383,14 +356,10 @@ $('a.event_edit').live('click', function(event) {
         var errString = validEvent(eventVals);
 
         //notify user or errors or submit form
-        if (errString.length)
-        {
+        if (errString.length) {
             notify(errString, true);
-        }
-        else
-        {
+        } else {
             var caseId = $(this).closest('.case_detail_panel').data('CaseNumber');
-
             var target = $(this).closest('.case_detail_panel_casenotes');
 
             $.post('lib/php/data/cases_events_process.php', {
@@ -405,24 +374,20 @@ $('a.event_edit').live('click', function(event) {
                 'case_id': caseId,
                 'event_id' : eventId
             }, function(data) {
-
                 var serverResponse = $.parseJSON(data);
-                if (serverResponse.error === true)
-                    {notify(serverResponse.message,true);}
-                        else
-                        {
-                            notify(serverResponse.message);
-                            target.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {'case_id': caseId},function(){
-                            //Scroll to original position of edited conact
-                            editedEvent = target.find('div.contact[data-id = "' + eventId + '"]');
-                            target.scrollTop(editEventPosition);
-
-                            });
-                        }
-
-                });
-
+                if (serverResponse.error === true) {
+                    notify(serverResponse.message,true);
+                } else {
+                    notify(serverResponse.message);
+                    target.load('lib/php/data/cases_events_load.php div.case_detail_panel_casenotes', {
+                        'case_id': caseId
+                    },function(){
+                        //Scroll to original position of edited conact
+                        var editedEvent = target.find('div.contact[data-id = "' + eventId + '"]');
+                        target.scrollTop(editEventPosition);
+                    });
+                }
+            });
         }
     });
-
 });
