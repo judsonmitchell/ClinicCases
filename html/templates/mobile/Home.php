72989588e4c7ba6192e5fbef1fede3bb685d0e63
Judson Mitchell <judsonmitchell@gmail.com>
2015-07-06 08:34:31 -0500

    Add media object for activities feed

--- html/templates/mobile/Home.php
+++ html/templates/mobile/Home.php
@@ -1,6 +1,6 @@
 </head>
 <body class="isMobile">
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
@@ -30,23 +30,26 @@
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
-</nav><div class="container">
-    <div class="row" id="notifications"></div>
-    <div class="row"><h1>Home</h1></div>
-    <?php include 'html/templates/interior/idletimeout.php' ?>
-    <?php include 'lib/php/data/home_activities_load.php' ?>
-    <div class="home-container row">
-        <div id="activities_feed">
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12" id="notifications"></div> 
+    </div>
+    <div class="row">
+        <div class="col-xs-12"> 
+        <h1>Home</h1>
+        <?php include 'html/templates/interior/idletimeout.php' ?>
+        <?php include 'lib/php/data/home_activities_load.php' ?>
             <?php if (empty($activities)) {
                 echo "<p class='end'>There has been no activity in the last sixty days. 
                 If you have just installed ClinicCases 7, it may take a while for this to start filling up.</p>"; die;
             }
 
             foreach ($activities as $activity) {
-                echo "<h5><img class='img-rounded' src='" . $activity['thumb'] . "'> " .
+                echo "<div class='media'><div class='media-left media-top'><img class='img-circle media-object' src='" . $activity['thumb'] . "'></div><div class='media-body'> <h4 class='media-heading'>" . 
                 $activity['by'] . $activity['action_text'] . "<a style='font-size:1em' href='" .  $activity['mobile_url'] .
-                "'>" . $activity['casename'] . "</a></h5><p>" . $activity['what'] .
-                "</p><p class='muted'>" . $activity['time_formatted'] . "</p><hr>";
+                "'>" . $activity['casename'] . "</a></h4><p>" . $activity['what'] .
+                "</p><p class='text-muted'>" . $activity['time_formatted'] . "</p></div></div>";
             }
             ?>
 
--- html/templates/mobile/Login.php
+++ html/templates/mobile/Login.php
@@ -5,7 +5,7 @@
 </head>
 
 <body  class="login">
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
