dba005d765a9335757898d7cbe1113a502d381ae
Judson Mitchell <judsonmitchell@gmail.com>
2015-08-16 08:21:04 -0500

    Completing escaping on Users

--- html/templates/interior/user_detail.php
+++ html/templates/interior/user_detail.php
@@ -6,7 +6,7 @@
 
 		<img class="thumbnail-mask"  src="<?php echo return_thumbnail($dbh,$username) . '?' . rand(); ?>">
 
-		<span class="name_display"><?php echo $first_name . " " . $last_name; ?></span>
+		<span class="name_display"><?php echo htmlspecialchars($first_name ,ENT_QUOTES,'UTF-8'). " " . htmlspecialchars($last_name,ENT_QUOTES,'UTF-8'); ?></span>
 
 	</p>
 
@@ -18,25 +18,25 @@
 
 <div class = "user_detail_left">
 
-	<p><label>First Name</label><?php echo $first_name; ?></p>
+	<p><label>First Name</label><?php echo htmlspecialchars($first_name,ENT_QUOTES,'UTF-8'); ?></p>
 
-	<p><label>Last Name</label><?php echo $last_name; ?></p>
+	<p><label>Last Name</label><?php echo htmlspecialchars($last_name,ENT_QUOTES,'UTF-8'); ?></p>
 
-	<p><label>Email</label><a href="mailto:<?php echo $email; ?>" target = "_new"><?php echo $email; ?> </a> </p>
+	<p><label>Email</label><a href="mailto:<?php echo htmlspecialchars($email,ENT_QUOTES,'UTF-8'); ?>" target = "_new"><?php echo htmlspecialchars($email,ENT_QUOTES,'UTF-8'); ?> </a> </p>
 
-	<p><label>Mobile Phone</label><?php echo $mobile_phone; ?></p>
+	<p><label>Mobile Phone</label><?php echo htmlspecialchars($mobile_phone,ENT_QUOTES,'UTF-8'); ?></p>
 
-	<p><label>Office Phone</label><?php echo $office_phone; ?></p>
+	<p><label>Office Phone</label><?php echo htmlspecialchars($office_phone,ENT_QUOTES,'UTF-8'); ?></p>
 
-	<p><label>Home Phone</label><?php echo $home_phone; ?></p>
+	<p><label>Home Phone</label><?php echo htmlspecialchars($home_phone,ENT_QUOTES,'UTF-8'); ?></p>
 
 	<p><label>Group</label><?php echo array_search($grp, $group_name_data); ?></p>
 
 	<p><label>Username</label><?php echo $username; ?></p>
 
-	<p><label>Supervisors</label><?php if ($supervisors){$sups = explode(',', $supervisors); $sup_names = array();foreach($sups as $sup){$sup_names[] = array_search($sup, $supervisor_name_data);} echo rtrim(implode(', ', $sup_names),', ');} ?></p>
+	<p><label>Supervisors</label><?php if ($supervisors){$sups = explode(',', $supervisors); $sup_names = array();foreach($sups as $sup){$sup_names[] = array_search($sup, $supervisor_name_data);} echo rtrim(implode(', ', htmlspecialchars($sup_names,ENT_QUOTES,'UTF-8')),', ');} ?></p>
 
-	<p><label>Status</label><?php echo $status; ?></p>
+	<p><label>Status</label><?php echo htmlspecialchars($status,ENT_QUOTES,'UTF-8'); ?></p>
 
 	<p><label>Date Created</label><?php echo extract_date_time($date_created); ?></p>
 
@@ -69,7 +69,7 @@
 			$ac = null;
 
 			foreach ($active_cases as $key => $value) {
-			  $ac .= "<a href='index.php?i=Cases.php#cases/$key' target='_new'>$value</a>" . ", ";
+			  $ac .= "<a href='index.php?i=Cases.php#cases/$key' target='_new'>" . htmlspecialchars($value ,ENT_QUOTES,'UTF-8'). "</a>" . ", ";
 			}
 
 			echo rtrim($ac,', ');
@@ -127,7 +127,7 @@
 
 		<img class="thumbnail-mask" src="<?php echo return_thumbnail($dbh,$username)  . '?' . rand(); ?>">
 
-		<span class="name_display"><?php echo $first_name . " " . $last_name; ?></span>
+		<span class="name_display"><?php echo htmlspecialchars($first_name ,ENT_QUOTES,'UTF-8'). " " . htmlspecialchars($last_name,ENT_QUOTES,'UTF-8'); ?></span>
 
 	</p>
 
@@ -142,17 +142,17 @@
 <div class = "user_detail_left">
 
 
-	<p><label>First Name</label><input name = "first_name" type="text" value="<?php echo $first_name; ?>"></p>
+	<p><label>First Name</label><input name = "first_name" type="text" value="<?php echo htmlspecialchars($first_name,ENT_QUOTES,'UTF-8'); ?>"></p>
 
-	<p><label>Last Name</label><input name = "last_name" type="text" value="<?php echo $last_name; ?>"></p>
+	<p><label>Last Name</label><input name = "last_name" type="text" value="<?php echo htmlspecialchars($last_name,ENT_QUOTES,'UTF-8'); ?>"></p>
 
-	<p><label>Email</label><input name = "email" type="text" value="<?php echo $email; ?>"></p>
+	<p><label>Email</label><input name = "email" type="text" value="<?php echo htmlspecialchars($email,ENT_QUOTES,'UTF-8'); ?>"></p>
 
-	<p><label>Mobile Phone</label><input name = "mobile_phone" type="text" value="<?php echo $mobile_phone; ?>"></p>
+	<p><label>Mobile Phone</label><input name = "mobile_phone" type="text" value="<?php echo htmlspecialchars($mobile_phone,ENT_QUOTES,'UTF-8'); ?>"></p>
 
-	<p><label>Office Phone</label><input name = "office_phone"  type="text" value="<?php echo $office_phone; ?>"></p>
+	<p><label>Office Phone</label><input name = "office_phone"  type="text" value="<?php echo htmlspecialchars($office_phone,ENT_QUOTES,'UTF-8'); ?>"></p>
 
-	<p><label>Home Phone</label><input name = "home_phone" type="text" value="<?php echo $home_phone; ?>"></p>
+	<p><label>Home Phone</label><input name = "home_phone" type="text" value="<?php echo htmlspecialchars($home_phone,ENT_QUOTES,'UTF-8'); ?>"></p>
 
 	<p><label>Group</label>
 
@@ -215,7 +215,7 @@
 
 				<label>Evaluations</label> <br />
 
-				<textarea name="evals" class="eval_block"><?php echo $evals; ?></textarea>
+				<textarea name="evals" class="eval_block"><?php echo htmlspecialchars($evals,ENT_QUOTES,'UTF-8'); ?></textarea>
 
 			</div>
 
