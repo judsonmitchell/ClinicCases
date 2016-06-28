7696a621f67bfec3a4506c38e72d709e7adc39bf
Judson Mitchell <judsonmitchell@gmail.com>
2016-06-28 13:36:34 -0500

    Fix to install script

--- _INSTALL/install_process.php
+++ _INSTALL/install_process.php
@@ -14,7 +14,7 @@ function deleteDir($dirPath) {
 
     foreach ($files as $file) {
         if (is_dir($file)) {
-            self::deleteDir($file);
+            $this->deleteDir($file);
         } else {
             unlink($file);
         }
