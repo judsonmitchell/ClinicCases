1545950333a2a1360f53dadf30722ef4dda824e3
test2a <39449563+test2a@users.noreply.github.com>
2021-05-21 10:01:54 +0530

    bypass mysql strict mode
    using here
    https://github.com/judsonmitchell/ClinicCases/issues/4#issuecomment-696192398

--- db.php
+++ db.php
@@ -1,14 +1,10 @@
 <?php
 require_once('_CONFIG.php');
-
 try {
-		$dbh = new PDO("mysql:host=" . CC_DBHOST . ";dbname=" . CC_DATABASE_NAME . ";charset=utf8mb4" , CC_DBUSERNAME, CC_DBPASSWD);
-    }
-catch(PDOException $e)
-    {
-
-		//400 is sent to trigger an error for ajax requests.
-		header('HTTP/1.1 400 Bad Request');
-
-		echo $e->getMessage();
-    }
+    $dbh = new PDO("mysql:host=" . CC_DBHOST . ";dbname=" . CC_DATABASE_NAME , CC_DBUSERNAME, CC_DBPASSWD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET sql_mode="IGNORE_SPACE,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"') );
+ }
+catch(PDOException $e) {
+    //400 is sent to trigger an error for ajax requests.
+    header('HTTP/1.1 400 Bad Request');
+    echo $e->getMessage();
+ }
